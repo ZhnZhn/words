a73733b3edeaa42c497300998a64ec5b2175bae3
Yevhenii Molodyi <eugmolody@gmail.com>
2022-02-07 15:08:08 +0200

    Edit SvgMore style

--- src/components/zhn-atoms/SvgMore.jsx
+++ src/components/zhn-atoms/SvgMore.jsx
@@ -1,40 +1,37 @@
-import { forwardRef }  from 'react'
+import { forwardRef }  from 'react';
 
-const CL = 'focusable';
-
-const S = {
-  BT: {
-    verticalAlign: 'middle',
-    paddingLeft: 6,
-    paddingRight: 6,
-    cursor: 'pointer'
-  },
-  SVG: {
-    fill: 'black',
-    stroke: 'black'
-  }
-};
-
-const T = "Click to open menu More";
+const CL_FOCUSABLE = 'focusable'
+, S_BT = {
+  verticalAlign: 'middle',
+  padding: '0 6px',
+  cursor: 'pointer'
+}
+, S_SVG = {
+  fill: 'black',
+  stroke: 'black'
+}
+, DF_TITLE = "Click to open menu More";
 
 const SvgMore = forwardRef(({
   style,
   svgStyle,
-  title=T,
+  title=DF_TITLE,
   onClick
 }, ref) => (
   <button
     ref={ref}
-    className={CL}
-    style={{...S.BT, ...style}}
+    className={CL_FOCUSABLE}
+    style={{...S_BT, ...style}}
     title={title}
     onClick={onClick}
   >
     <svg
-      style={{...S.SVG, ...svgStyle}}
-      width="6px" height="22px"
+      style={{...S_SVG, ...svgStyle}}
+      width="6px"
+      height="22px"
       viewBox="0 0 6 22"
-      preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
       <circle cx="3" cy="4" r="2" />
       <circle cx="3" cy="11" r="2" />
