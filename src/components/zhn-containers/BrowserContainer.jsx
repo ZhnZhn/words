cab9789996c3ac7c3a143dbfc6c9550f53f830ab
Yevhenii Molodyi <eugmolody@gmail.com>
2022-03-01 11:54:54 +0200

    Replace BrowserContainer by function component

--- src/components/zhn-containers/BrowserContainer.jsx
+++ src/components/zhn-containers/BrowserContainer.jsx
@@ -1,46 +1,40 @@
-import { Component } from 'react'
-
-import MenuBrowser from '../zhn-browsers/MenuBrowser'
-import WatchBrowser from '../watch-browser/WatchBrowser'
-import DialogContainer from './DialogContainer'
+import MenuBrowser from '../zhn-browsers/MenuBrowser';
+import WatchBrowser from '../watch-browser/WatchBrowser';
+import DialogContainer from './DialogContainer';
 
 const CL_ROOT = "hrz-container";
 
-class BrowserContainer extends Component {
-  render(){
-    const {
-             store,
-             browserId,
-             showBrowserAction, showDialogAction,
-             onClickItem,
-             updateWatchAction,
-             onClickWatchItem
-           } = this.props;
-    return (
-      <div className={CL_ROOT}>
-        <MenuBrowser
-          store={store}
-          browserId={browserId}
-          showAction={showBrowserAction}
-          onClickItem={onClickItem}
-        />
-        <WatchBrowser
-          caption="Watch Words"
-          store={store}
-          isInitShow={false}
-          browserType="WATCH_ID"
-          showAction={showBrowserAction}
-          updateAction={updateWatchAction}
-          onClickItem={onClickWatchItem}
-        />
-        <DialogContainer
-          maxDialog={3}
-          store={store}
-          showAction={showDialogAction}
-        />
-      </div>
-    );
-  }
-}
+const BrowserContainer = ({
+  store,
+  browserId,
+  showBrowserAction,
+  showDialogAction,
+  onClickItem,
+  updateWatchAction,
+  onClickWatchItem
+}) => (
+  <div className={CL_ROOT}>
+    <MenuBrowser
+      store={store}
+      browserId={browserId}
+      showAction={showBrowserAction}
+      onClickItem={onClickItem}
+    />
+    <WatchBrowser
+      caption="Watch Words"
+      store={store}
+      isInitShow={false}
+      browserType="WATCH_ID"
+      showAction={showBrowserAction}
+      updateAction={updateWatchAction}
+      onClickItem={onClickWatchItem}
+    />
+    <DialogContainer
+      maxDialog={3}
+      store={store}
+      showAction={showDialogAction}
+    />
+  </div>
+);
 
 export default BrowserContainer
