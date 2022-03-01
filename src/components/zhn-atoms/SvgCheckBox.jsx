ce91980eb80103c8782baa553e465bf1830691d6
Yevhenii Molodyi <eugmolody@gmail.com>
2022-03-01 15:21:07 +0200

    Replace SvgCheckBox by function component

--- src/components/zhn-atoms/SvgCheckBox.jsx
+++ src/components/zhn-atoms/SvgCheckBox.jsx
@@ -1,115 +1,90 @@
-import { Component } from 'react';
-//import PropTypes from "prop-types";
+import { useState } from '../uiApi';
 
-import isKeyEnter from './isKeyEnter'
+import isKeyEnter from './isKeyEnter';
 import C from '../styles/Color';
 
-
-//const DF_COLOR_IS = "#80c040";
 const DF_COLOR_IS = "#2f7ed8"
+, S_DIV = {
+  display: 'inline-block',
+  width: 16,
+  height: 16,
+  cursor: 'pointer'
+}
+, S_SVG = { display: 'inline-block' };
 
-const S = {
-  DIV : {
-    display: 'inline-block',
-    width: 16,
-    height: 16,
-    cursor: 'pointer'
-  },
-  SVG : {
-    display: 'inline-block'
-  }
-};
 
 const SvgChecked = ({ stroke }) => (
   <path
-      //d="M 2,3 L 8,14 14,3"
-      d="M 2,5 L 8,14 14,1"
-      strokeWidth="2"
-      strokeLinecap="round"
-      stroke={stroke}
-      fill={C.BLANK}
+     d="M 2,5 L 8,14 14,1"
+     strokeWidth="2"
+     strokeLinecap="round"
+     stroke={stroke}
+     fill={C.BLANK}
   />
 );
 
 const _isFn = fn => typeof fn === 'function';
 
-class SvgCheckBox extends Component {
-  /*
-  static propTypes = {
-    initialValue: PropTypes.bool,
-    stroke: PropTypes.string,
-    onCheck: PropTypes.func,
-    onUnCheck: PropTypes.func
-  }
-  */
-
-  constructor(props){
-    super(props);
-    const { initialValue } = props;
-
-    this.state = {
-        isChecked: !!initialValue,
+const SvgCheckBox = ({
+  initialValue,
+  style,
+  stroke,
+  onCheck,
+  onUnCheck
+}) => {
+  const [
+    isChecked,
+    setIsChecked
+  ] = useState(() => !!initialValue)
+  , _hClick = () => {
+      if (!isChecked && _isFn(onCheck)){
+        onCheck();
+      } else if (_isFn(onUnCheck)){
+        onUnCheck();
+      }
+      setIsChecked(v => !v)
     }
+  , _hKeyDown = (evt) => {
+      if (isKeyEnter(evt)){
+        evt.preventDefault()
+        _hClick()
+      }
   }
+  , _restProps = isChecked
+      ? { stroke: DF_COLOR_IS, fill: DF_COLOR_IS}
+      : { stroke: C.GREY, fill: C.BLANK };
 
-
-  _hClick = () => {
-    const { onCheck, onUnCheck } = this.props
-    , { isChecked } = this.state;
-    if (!isChecked && _isFn(onCheck)){
-      onCheck(this);
-    } else if (_isFn(onUnCheck)){
-      onUnCheck(this);
-    }
-    this.setState({ isChecked: !isChecked });
-  }
-
-  _hKeyDown = (evt) => {
-    if (isKeyEnter(evt)){
-      evt.preventDefault()
-      this._hClick()
-    }
-  }
-
-  render(){
-    const { style, stroke } = this.props
-    , { isChecked } = this.state
-    , _restProps = isChecked
-        ? { stroke: DF_COLOR_IS, fill: DF_COLOR_IS}
-        : { stroke: C.GREY, fill: C.BLANK };
-    return (
-      <div
-         role="checkbox"
-         tabIndex="0"
-         aria-checked={isChecked}
-         style={{ ...S.DIV, ...style }}
-         onClick={this._hClick}
-         onKeyDown={this._hKeyDown}
+  return (
+    <div
+       role="checkbox"
+       tabIndex="0"
+       aria-checked={isChecked}
+       style={{...S_DIV, ...style}}
+       onClick={_hClick}
+       onKeyDown={_hKeyDown}
+    >
+      <svg
+         viewBox="0 0 16 16"
+         width="100%"
+         height="100%"
+         preserveAspectRatio="none"
+         xmlns="http://www.w3.org/2000/svg"
+         style={S_SVG}
       >
-        <svg
-            viewBox="0 0 16 16" width="100%" height="100%"
-            preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
-            style={S.SVG}
-        >
-          <rect
-             x="1" y="1"
-             height="14" width="14"
-             strokeWidth="2" rx="3"
-             strokeLinecap="round"
-             {..._restProps}
-          />
-          { isChecked
-              ? <SvgChecked stroke={stroke} />
-              : null
-          }
-        </svg>
-      </div>
-    );
-  }
-
-  setUnchecked = () => {
-    this.setState({ isChecked: false });
-  }
-}
+        <rect
+           x="1" y="1"
+           height="14" width="14"
+           strokeWidth="2" rx="3"
+           strokeLinecap="round"
+           {..._restProps}
+        />
+        { isChecked
+            ? <SvgChecked stroke={stroke} />
+            : null
+        }
+      </svg>
+    </div>
+  );
+};
 
 export default SvgCheckBox
