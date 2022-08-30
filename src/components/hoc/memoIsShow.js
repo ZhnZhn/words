ada259809d60bf5a021cf1a98bd6b9cd55c1b61e
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-30 17:40:27 +0300

    Add hoc memoIsShow

---
+++ src/components/hoc/memoIsShow.js
@@ -0,0 +1,9 @@
+import { memo } from '../uiApi';
+
+const _isNotShouldRerender = (
+  prevProps,
+  nextProps
+) => prevProps.isShow === nextProps.isShow
+const memoIsShow = Comp => memo(Comp, _isNotShouldRerender)
+
+export default memoIsShow
