3e8958bb68a1b4b4b5e9639a4d690fcf39b4b412
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-31 11:13:24 +0300

    Replace ItemHeader class component by function component

--- js/components/items/ItemHeader.js
+++ js/components/items/ItemHeader.js
@@ -5,142 +5,110 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
-var _react = require("react");
+var _uiApi = require("../uiApi");
 
 var _Comp = _interopRequireDefault(require("../Comp"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var CL_NOT_SELECTED = "not-selected";
-var T = {
-  A: "Click to open add to watch list dialog"
-};
-
-var _fnNoop = function _fnNoop() {};
+var CL_NOT_SELECTED = "not-selected",
+    TITLE = "Click to open add to watch list dialog";
 
 var _setPrevFocused = function _setPrevFocused(element) {
   document._prevFocusedZhn = element;
 };
 
-var ItemHeader = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(ItemHeader, _Component);
+var FN_NOOP = function FN_NOOP() {};
 
-  /*
-  static propTypes = {
-    title: PropTypes.string,
-    className: PropTypes.string,
-    style: PropTypes.object,
-    captionStyle: PropTypes.object,
-    svgCloseStyle: PropTypes.object,
-      caption: PropTypes.string,
-      onClick: PropTypes.func,
-    onClose: PropTypes.func,
-    onAddToWatch: PropTypes.func
-  }
-  */
-  function ItemHeader(props) {
-    var _this;
+var ItemHeader = (0, _uiApi.forwardRef)(function (_ref, ref) {
+  var className = _ref.className,
+      style = _ref.style,
+      captionStyle = _ref.captionStyle,
+      svgCloseStyle = _ref.svgCloseStyle,
+      title = _ref.title,
+      caption = _ref.caption,
+      _ref$onAddToWatch = _ref.onAddToWatch,
+      onAddToWatch = _ref$onAddToWatch === void 0 ? FN_NOOP : _ref$onAddToWatch,
+      _ref$onClose = _ref.onClose,
+      onClose = _ref$onClose === void 0 ? FN_NOOP : _ref$onClose,
+      _ref$onClick = _ref.onClick,
+      onClick = _ref$onClick === void 0 ? FN_NOOP : _ref$onClick;
 
-    _this = _Component.call(this, props) || this;
+  var _refRootNode = (0, _uiApi.useRef)(),
+      _refBtAdd = (0, _uiApi.useRef)(),
+      _hAddToWatch = (0, _uiApi.useCallback)(function (evt) {
+    evt.stopPropagation();
 
-    _this._hAddToWatch = function (event) {
-      event.stopPropagation();
+    _setPrevFocused((0, _uiApi.getRefValue)(_refBtAdd));
 
-      _setPrevFocused(_this._refBtAdd.current);
-
-      var _this$props = _this.props,
-          caption = _this$props.caption,
-          onAddToWatch = _this$props.onAddToWatch;
-      onAddToWatch({
-        caption: caption
-      });
-    };
-
-    _this._hClose = function (event) {
-      event.stopPropagation();
-
-      _this.props.onClose();
-    };
-
-    _this._hKeyDown = function (event) {
-      var target = event.target,
-          keyCode = event.keyCode;
-
-      if (target === _this._refRootNode.current) {
-        var _this$props2 = _this.props,
-            onClick = _this$props2.onClick,
-            onClose = _this$props2.onClose;
-
-        if (keyCode === 13) {
-          onClick();
-        } else if (keyCode === 46) {
-          onClose();
-        } else if (keyCode === 65) {
-          _this._hAddToWatch(event);
-        }
-      }
-    };
-
-    _this._refRootNode = /*#__PURE__*/(0, _react.createRef)();
-    _this._refBtAdd = /*#__PURE__*/(0, _react.createRef)();
-    return _this;
-  }
-
-  var _proto = ItemHeader.prototype;
-
-  _proto.componentDidMount = function componentDidMount() {
-    if (this._refRootNode.current) {
-      this._refRootNode.current.focus();
-    }
-  };
-
-  _proto.render = function render() {
-    var _this$props3 = this.props,
-        className = _this$props3.className,
-        style = _this$props3.style,
-        captionStyle = _this$props3.captionStyle,
-        svgCloseStyle = _this$props3.svgCloseStyle,
-        title = _this$props3.title,
-        onClick = _this$props3.onClick;
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-      tabIndex: "0",
-      role: "button",
-      ref: this._refRootNode,
-      className: className,
-      style: style,
-      onClick: onClick,
-      onKeyDown: this._hKeyDown,
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-        className: CL_NOT_SELECTED,
-        style: captionStyle,
-        children: title
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].CircleButton, {
-        ref: this._refBtAdd,
-        caption: "A",
-        title: T.A,
-        onClick: this._hAddToWatch
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].SvgClose, {
-        tabIndex: "-1",
-        style: svgCloseStyle,
-        onClose: this._hClose
-      })]
+    onAddToWatch({
+      caption: caption
     });
-  };
+  }, [caption, onAddToWatch]),
+      _hClose = (0, _uiApi.useCallback)(function (evt) {
+    evt.stopPropagation();
+    onClose();
+  }, [onClose]),
+      _hKeyDown = (0, _uiApi.useCallback)(function (evt) {
+    var target = evt.target,
+        keyCode = evt.keyCode;
 
-  _proto.focus = function focus() {
-    this._refRootNode.current.focus();
-  };
+    if (target === (0, _uiApi.getRefValue)(_refRootNode)) {
+      if (keyCode === 13) {
+        onClick();
+      } else if (keyCode === 46) {
+        onClose();
+      } else if (keyCode === 65) {
+        _hAddToWatch(event);
+      }
+    }
+  }, [_hAddToWatch, onClick, onClose]),
+      focus = (0, _uiApi.useCallback)(function () {
+    var _elRoot = (0, _uiApi.getRefValue)(_refRootNode);
 
-  return ItemHeader;
-}(_react.Component);
+    if (_elRoot) {
+      _elRoot.focus();
+    }
+  }, []);
+  /*eslint-disable react-hooks/exhaustive-deps */
 
-ItemHeader.defaultProps = {
-  onClick: _fnNoop,
-  onClose: _fnNoop,
-  onAddToWatch: _fnNoop
-};
+
+  (0, _uiApi.useEffect)(function () {
+    focus();
+  }, []); // focus
+
+  (0, _uiApi.useImperativeHandle)(ref, function () {
+    return {
+      focus: focus
+    };
+  }, []); // focus
+
+  /*eslint-enable react-hooks/exhaustive-deps */
+
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    tabIndex: "0",
+    role: "button",
+    ref: _refRootNode,
+    className: className,
+    style: style,
+    onClick: onClick,
+    onKeyDown: _hKeyDown,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+      className: CL_NOT_SELECTED,
+      style: captionStyle,
+      children: title
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].CircleButton, {
+      ref: _refBtAdd,
+      caption: "A",
+      title: TITLE,
+      onClick: _hAddToWatch
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Comp["default"].SvgClose, {
+      tabIndex: "-1",
+      style: svgCloseStyle,
+      onClose: _hClose
+    })]
+  });
+});
 var _default = ItemHeader;
 exports["default"] = _default;
 //# sourceMappingURL=ItemHeader.js.map
--- src/components/items/ItemHeader.jsx
+++ src/components/items/ItemHeader.jsx
@@ -1,123 +1,105 @@
-import { Component, createRef } from 'react'
+import {
+  forwardRef,
+  useRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  getRefValue
+} from '../uiApi'
 
-import A from '../Comp'
+import A from '../Comp';
 
-const CL_NOT_SELECTED = "not-selected";
-
-const T = {
-  A: "Click to open add to watch list dialog"
-};
-
-const _fnNoop = () => {};
+const CL_NOT_SELECTED = "not-selected"
+, TITLE = "Click to open add to watch list dialog";
 
 const _setPrevFocused = element => {
   document._prevFocusedZhn = element
 };
 
-class ItemHeader extends Component {
+const FN_NOOP = () => {};
 
-  /*
-  static propTypes = {
-    title: PropTypes.string,
-    className: PropTypes.string,
-    style: PropTypes.object,
-    captionStyle: PropTypes.object,
-    svgCloseStyle: PropTypes.object,
+const ItemHeader = forwardRef(({
+  className,
+  style,
+  captionStyle,
+  svgCloseStyle,
+  title,
+  caption,
+  onAddToWatch=FN_NOOP,
+  onClose=FN_NOOP,
+  onClick=FN_NOOP
+}, ref) => {
+  const _refRootNode = useRef()
+  , _refBtAdd = useRef()
+  , _hAddToWatch = useCallback(evt => {
+     evt.stopPropagation()
+     _setPrevFocused(getRefValue(_refBtAdd))
+     onAddToWatch({ caption })
+  }, [caption, onAddToWatch])
+  , _hClose = useCallback(evt => {
+     evt.stopPropagation()
+     onClose()
+  }, [onClose])
+  , _hKeyDown = useCallback(evt => {
+     const { target, keyCode } = evt;
+     if (target === getRefValue(_refRootNode)) {
+       if (keyCode === 13) {
+         onClick()
+       } else if (keyCode === 46) {
+         onClose()
+       } else if (keyCode === 65) {
+         _hAddToWatch(event)
+       }
+     }
+  }, [_hAddToWatch, onClick, onClose])
+  , focus = useCallback(() => {
+     const _elRoot = getRefValue(_refRootNode);
+     if (_elRoot) {
+       _elRoot.focus()
+     }
+  }, []);
 
-    caption: PropTypes.string,
+  /*eslint-disable react-hooks/exhaustive-deps */
+  useEffect(()=>{
+    focus()
+  }, [])
+  // focus
 
-    onClick: PropTypes.func,
-    onClose: PropTypes.func,
-    onAddToWatch: PropTypes.func
-  }
-  */
+  useImperativeHandle(ref, () => ({
+    focus
+  }), [])
+  // focus
+  /*eslint-enable react-hooks/exhaustive-deps */
 
-  static defaultProps = {
-    onClick: _fnNoop,
-    onClose: _fnNoop,
-    onAddToWatch: _fnNoop
-  }
-
-  constructor(props){
-    super(props)
-    this._refRootNode = createRef()
-    this._refBtAdd = createRef()
-  }
-
-
-  componentDidMount(){
-    if (this._refRootNode.current) {
-      this._refRootNode.current.focus()
-    }
-  }
-
-  _hAddToWatch = (event) => {
-    event.stopPropagation()
-    _setPrevFocused(this._refBtAdd.current)
-    const { caption, onAddToWatch } = this.props
-    onAddToWatch({ caption })
-  }
-
-  _hClose = (event) => {
-    event.stopPropagation()
-    this.props.onClose()
-  }
-
-  _hKeyDown = (event) => {
-    const { target, keyCode } = event;
-    if (target === this._refRootNode.current) {
-      const { onClick, onClose } = this.props;
-      if (keyCode === 13) {
-        onClick()
-      } else if (keyCode === 46) {
-        onClose()
-      } else if (keyCode === 65) {
-        this._hAddToWatch(event)
-      }
-    }
-  }
-
-  render(){
-    const {
-            className,
-            style, captionStyle, svgCloseStyle,
-            title,
-            onClick
-          } = this.props;
-    return (
-      <div
-        tabIndex="0"
-        role="button"
-        ref={this._refRootNode}
-        className={className}
-        style={style}
-        onClick={onClick}
-        onKeyDown={this._hKeyDown}
+  return (
+    <div
+      tabIndex="0"
+      role="button"
+      ref={_refRootNode}
+      className={className}
+      style={style}
+      onClick={onClick}
+      onKeyDown={_hKeyDown}
+    >
+      <span
+         className={CL_NOT_SELECTED}
+         style={captionStyle}
       >
-        <span
-           className={CL_NOT_SELECTED}
-           style={captionStyle}
-        >
-           {title}
-        </span>
-        <A.CircleButton
-          ref={this._refBtAdd}
-          caption="A"
-          title={T.A}
-          onClick={this._hAddToWatch}
-        />
-        <A.SvgClose
-            tabIndex="-1"
-            style={svgCloseStyle}
-            onClose={this._hClose}
-        />
-      </div>
-    );
-  }
-
-  focus() {
-    this._refRootNode.current.focus()
-  }
-}
+         {title}
+      </span>
+      <A.CircleButton
+        ref={_refBtAdd}
+        caption="A"
+        title={TITLE}
+        onClick={_hAddToWatch}
+      />
+      <A.SvgClose
+          tabIndex="-1"
+          style={svgCloseStyle}
+          onClose={_hClose}
+      />
+    </div>
+  );
+});
 
 export default ItemHeader
