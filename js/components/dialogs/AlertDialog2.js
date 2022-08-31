8c1ba25a2b79bd02a8a67d947d6d6104dc2ab6b0
Yevhenii Molodyi <eugmolody@gmail.com>
2022-08-31 12:10:41 +0300

    Replace AlertDialog2 class component by function component

--- js/components/dialogs/AlertDialog2.js
+++ js/components/dialogs/AlertDialog2.js
@@ -7,11 +7,9 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
+var _memoIsShow = _interopRequireDefault(require("../hoc/memoIsShow"));
 
-var _react = require("react");
-
-var _withTheme = _interopRequireDefault(require("../hoc/withTheme"));
+var _useTheme = _interopRequireDefault(require("../hoc/useTheme"));
 
 var _Dialog = _interopRequireDefault(require("./Dialog.Style"));
 
@@ -20,131 +18,102 @@ var _Comp = _interopRequireDefault(require("../Comp"));
 var _jsxRuntime = require("react/jsx-runtime");
 
 //import PropTypes from "prop-types";
-var CL = {
-  ELL: 'ellipsis'
+var CL_ELL = 'ellipsis',
+    S_DIALOG = {
+  left: 'calc(50vw - 184px)'
+},
+    S_CAPTION = {
+  color: '#f44336',
+  fontWeight: 'bold'
+},
+    S_ROW = {
+  display: 'flex',
+  alignItems: 'center',
+  marginRight: 5,
+  marginTop: 5,
+  marginLeft: 5,
+  marginBottom: 5
+},
+    S_TITLE = {
+  display: 'inline-block',
+  color: '#f44336',
+  width: 360,
+  paddingLeft: 10,
+  lineHeight: 2,
+  fontSize: '18px',
+  fontWeight: 'bold'
+},
+    S_ITEM_ID = {
+  color: '#a487d4',
+  width: 120,
+  fontWeight: 'bold',
+  verticalAlign: 'bottom'
+},
+    S_DESCR = {
+  color: 'gray',
+  width: 360,
+  paddingLeft: 10,
+  paddingRight: 8,
+  fontWeight: 'bold',
+  lineHeight: 1.4,
+  whiteSpace: 'pre-line',
+  wordWrap: 'break-word'
 };
-var S = {
-  DIALOG: {
-    left: 'calc(50vw - 184px)'
-  },
-  CAPTION: {
-    color: '#f44336',
-    fontWeight: 'bold'
-  },
-  ROW: {
-    display: 'flex',
-    alignItems: 'center',
-    marginRight: 5,
-    marginTop: 5,
-    marginLeft: 5,
-    marginBottom: 5
-  },
-  TITLE: {
-    display: 'inline-block',
-    color: '#f44336',
-    width: 360,
-    paddingLeft: 10,
-    lineHeight: 2,
-    fontSize: '18px',
-    fontWeight: 'bold'
-  },
-  ITEM_ID: {
-    color: '#a487d4',
-    width: 120,
-    fontWeight: 'bold',
-    verticalAlign: 'bottom'
-  },
-  DESCR: {
-    color: 'gray',
-    width: 360,
-    paddingLeft: 10,
-    paddingRight: 8,
-    fontWeight: 'bold',
-    lineHeight: 1.4,
-    whiteSpace: 'pre-line',
-    wordWrap: 'break-word'
-  }
-};
+var DF_DATA = {};
+var AlertDialog2 = (0, _memoIsShow["default"])(function (_ref) {
+  var isShow = _ref.isShow,
+      _ref$data = _ref.data,
+      data = _ref$data === void 0 ? DF_DATA : _ref$data,
+      onClose = _ref.onClose;
 
-var AlertDialog2 = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(AlertDialog2, _Component);
+  var TS = (0, _useTheme["default"])(_Dialog["default"]),
+      _data$caption = data.caption,
+      caption = _data$caption === void 0 ? 'Item' : _data$caption,
+      _data$itemId = data.itemId,
+      itemId = _data$itemId === void 0 ? '' : _data$itemId,
+      descr = data.descr,
+      _caption = caption + ': ';
 
-  function AlertDialog2() {
-    return _Component.apply(this, arguments) || this;
-  }
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_Comp["default"].ModalDialog, {
+    STYLE: TS.BT,
+    style: (0, _extends2["default"])({}, TS.R_DIALOG, S_DIALOG),
+    caption: "Exception",
+    captionStyle: (0, _extends2["default"])({}, TS.BROWSER_CAPTION, S_CAPTION),
+    isShow: isShow,
+    isClosePrimary: true,
+    onClose: onClose,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      style: S_ROW,
+      children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
+        style: S_TITLE,
+        children: [_caption, /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+          className: CL_ELL,
+          style: S_ITEM_ID,
+          title: itemId,
+          children: itemId
+        })]
+      })
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      style: S_ROW,
+      children: /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+        style: S_DESCR,
+        children: descr
+      })
+    })]
+  });
+});
+/*
+AlertDialog2.propTypes = {
+  isShow: PropTypes.bool,
+  data: PropTypes.shape({
+    caption: PropTypes.string,
+    itemId: PropTypes.string,
+    descr: PropTypes.string
+  }),
+  onClose: PropTypes.func
+}
+*/
 
-  var _proto = AlertDialog2.prototype;
-
-  /*
-  static propTypes = {
-    isShow: PropTypes.bool,
-    data: PropTypes.shape({
-      caption: PropTypes.string,
-      itemId: PropTypes.string,
-      descr: PropTypes.string
-    }),
-    onClose: PropTypes.func
-  }
-  */
-  _proto.shouldComponentUpdate = function shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps !== this.props && nextProps.isShow === this.props.isShow) {
-      return false;
-    }
-
-    return true;
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        theme = _this$props.theme,
-        isShow = _this$props.isShow,
-        data = _this$props.data,
-        onClose = _this$props.onClose,
-        TS = theme.createStyle(_Dialog["default"]),
-        _data$caption = data.caption,
-        caption = _data$caption === void 0 ? 'Item' : _data$caption,
-        _data$itemId = data.itemId,
-        itemId = _data$itemId === void 0 ? '' : _data$itemId,
-        descr = data.descr,
-        _caption = caption + ': ';
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_Comp["default"].ModalDialog, {
-      STYLE: TS.BT,
-      style: (0, _extends2["default"])({}, TS.R_DIALOG, S.DIALOG),
-      caption: "Exception",
-      captionStyle: (0, _extends2["default"])({}, TS.BROWSER_CAPTION, S.CAPTION),
-      isShow: isShow,
-      isClosePrimary: true,
-      onClose: onClose,
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-        style: S.ROW,
-        children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("span", {
-          style: S.TITLE,
-          children: [_caption, /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-            className: CL.ELL,
-            style: S.ITEM_ID,
-            title: itemId,
-            children: itemId
-          })]
-        })
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-        style: S.ROW,
-        children: /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
-          style: S.DESCR,
-          children: descr
-        })
-      })]
-    });
-  };
-
-  return AlertDialog2;
-}(_react.Component);
-
-AlertDialog2.defaultProps = {
-  data: {}
-};
-
-var _default = (0, _withTheme["default"])(AlertDialog2);
-
+var _default = AlertDialog2;
 exports["default"] = _default;
 //# sourceMappingURL=AlertDialog2.js.map
--- src/components/dialogs/AlertDialog2.jsx
+++ src/components/dialogs/AlertDialog2.jsx
@@ -1,119 +1,110 @@
-import { Component } from 'react';
 //import PropTypes from "prop-types";
 
-import withTheme from '../hoc/withTheme'
-import styleConfig from './Dialog.Style'
+import memoIsShow from '../hoc/memoIsShow';
+import useTheme from '../hoc/useTheme';
+import styleConfig from './Dialog.Style';
 
 import A from '../Comp'
 
+const CL_ELL = 'ellipsis'
 
-const CL = {
-  ELL: 'ellipsis'
+, S_DIALOG = {
+  left: 'calc(50vw - 184px)'
+}
+, S_CAPTION = {
+  color: '#f44336',
+  fontWeight: 'bold'
+}
+, S_ROW = {
+  display: 'flex',
+  alignItems: 'center',
+  marginRight: 5,
+  marginTop: 5,
+  marginLeft: 5,
+  marginBottom: 5
+}
+, S_TITLE = {
+  display : 'inline-block',
+  color : '#f44336',
+  width : 360,
+  paddingLeft : 10,
+  lineHeight : 2,
+  fontSize: '18px',
+  fontWeight : 'bold'
+}
+, S_ITEM_ID = {
+  color: '#a487d4',
+  width: 120,
+  fontWeight : 'bold',
+  verticalAlign: 'bottom'
+}
+, S_DESCR = {
+  color: 'gray',
+  width: 360,
+  paddingLeft: 10,
+  paddingRight: 8,
+  fontWeight: 'bold',
+  lineHeight: 1.4,
+  whiteSpace: 'pre-line',
+  wordWrap: 'break-word'
 };
 
-const S = {
-  DIALOG: {
-    left: 'calc(50vw - 184px)'
-  },
-  CAPTION: {
-    color: '#f44336',
-    fontWeight: 'bold'
-  },
-  ROW: {
-    display: 'flex',
-    alignItems: 'center',
-    marginRight: 5,
-    marginTop: 5,
-    marginLeft: 5,
-    marginBottom: 5
-  },
-  TITLE : {
-    display : 'inline-block',
-    color : '#f44336',
-    width : 360,
-    paddingLeft : 10,
-    lineHeight : 2,
-    fontSize: '18px',
-    fontWeight : 'bold'
-  },
-  ITEM_ID : {
-    color: '#a487d4',
-    width: 120,
-    fontWeight : 'bold',
-    verticalAlign: 'bottom'
-  },
-  DESCR: {
-    color: 'gray',
-    width: 360,
-    paddingLeft: 10,
-    paddingRight: 8,
-    fontWeight: 'bold',
-    lineHeight: 1.4,
-    whiteSpace: 'pre-line',
-    wordWrap: 'break-word'
-  }
-}
+const DF_DATA = {};
 
-class AlertDialog2 extends Component{
+const AlertDialog2 = memoIsShow(({
+  isShow,
+  data=DF_DATA,
+  onClose
+}) => {
+  const TS = useTheme(styleConfig)
+  , {
+    caption='Item',
+    itemId='',
+    descr
+  } = data
+  , _caption = caption + ': ';
 
-  /*
-  static propTypes = {
-    isShow: PropTypes.bool,
-    data: PropTypes.shape({
-      caption: PropTypes.string,
-      itemId: PropTypes.string,
-      descr: PropTypes.string
-    }),
-    onClose: PropTypes.func
-  }
-  */
-
-  static defaultProps = {
-    data: {}
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    if (nextProps !== this.props && nextProps.isShow === this.props.isShow) {
-      return false;
-    }
-    return true;
-  }
-
-  render(){
-    const { theme, isShow, data, onClose } = this.props
-        , TS = theme.createStyle(styleConfig)
-        , { caption='Item', itemId='', descr } = data
-        , _caption = caption + ': ';
-    return (
-      <A.ModalDialog
-        STYLE={TS.BT}
-        style={{...TS.R_DIALOG, ...S.DIALOG }}
-        caption="Exception"
-        captionStyle={{ ...TS.BROWSER_CAPTION, ...S.CAPTION }}
-        isShow={isShow}
-        isClosePrimary={true}
-        onClose={onClose}
-      >
-         <div style={S.ROW}>
-            <span style={S.TITLE}>
-              {_caption}
-              <span
-                className={CL.ELL}
-                style={S.ITEM_ID}
-                title={itemId}
-              >
-                {itemId}
-              </span>
+  return (
+    <A.ModalDialog
+      STYLE={TS.BT}
+      style={{...TS.R_DIALOG, ...S_DIALOG }}
+      caption="Exception"
+      captionStyle={{ ...TS.BROWSER_CAPTION, ...S_CAPTION }}
+      isShow={isShow}
+      isClosePrimary={true}
+      onClose={onClose}
+    >
+       <div style={S_ROW}>
+          <span style={S_TITLE}>
+            {_caption}
+            <span
+              className={CL_ELL}
+              style={S_ITEM_ID}
+              title={itemId}
+            >
+              {itemId}
             </span>
-         </div>
-         <div style={S.ROW}>
-            <p style={S.DESCR}>
-              {descr}
-            </p>
-         </div>
-      </A.ModalDialog>
-    );
-  }
-}
+          </span>
+       </div>
+       <div style={S_ROW}>
+          <p style={S_DESCR}>
+            {descr}
+          </p>
+       </div>
+    </A.ModalDialog>
+  );
+});
 
-export default withTheme(AlertDialog2)
+/*
+AlertDialog2.propTypes = {
+  isShow: PropTypes.bool,
+  data: PropTypes.shape({
+    caption: PropTypes.string,
+    itemId: PropTypes.string,
+    descr: PropTypes.string
+  }),
+  onClose: PropTypes.func
+}
+*/
+
+export default AlertDialog2
