7612bffb189f07d4a236bcbcb3d126eb3515d8d7
Yevhenii Molodyi <eugmolody@gmail.com>
2022-09-02 09:58:22 +0300

    Add css flex style to RowButtons

--- js/components/watch-browser/RowButtons.js
+++ js/components/watch-browser/RowButtons.js
@@ -5,11 +5,17 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+
 var _Button = _interopRequireDefault(require("./Button"));
 
-var _Pane = _interopRequireDefault(require("./Pane.Style"));
+var _GeneralStyles = require("../styles/GeneralStyles");
 
 var _jsxRuntime = require("react/jsx-runtime");
+
+var S_DIV = (0, _extends2["default"])({}, _GeneralStyles.S_FLEX_ROW_END, {
+  margin: '8px 4px 6px 0'
+});
 
 var RowButtons = function RowButtons(_ref) {
   var btStyle = _ref.btStyle,
@@ -18,7 +24,7 @@ var RowButtons = function RowButtons(_ref) {
       onClear = _ref.onClear,
       onClose = _ref.onClose;
   return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-    style: _Pane["default"].COMMAND_DIV,
+    style: S_DIV,
     children: [Primary, !withoutClear && /*#__PURE__*/(0, _jsxRuntime.jsx)(_Button["default"].Clear, {
       style: btStyle,
       onClick: onClear
--- src/components/watch-browser/RowButtons.jsx
+++ src/components/watch-browser/RowButtons.jsx
@@ -1,21 +1,33 @@
-import Button from './Button'
-import S from './Pane.Style'
+import Button from './Button';
+import {
+  S_FLEX_ROW_END
+} from '../styles/GeneralStyles';
 
-const RowButtons = ({ btStyle, Primary, withoutClear, onClear, onClose }) => (
-  <div style={S.COMMAND_DIV}>
+const S_DIV = {
+  ...S_FLEX_ROW_END,
+  margin: '8px 4px 6px 0'
+};
+
+const RowButtons = ({
+  btStyle,
+  Primary,
+  withoutClear,
+  onClear,
+  onClose
+}) => (
+  <div style={S_DIV}>
     {Primary}
     {!withoutClear &&
-      <Button.Clear
-         style={btStyle}
-         onClick={onClear}
-      />
+     <Button.Clear
+        style={btStyle}
+        onClick={onClear}
+     />
     }
     <Button.Close
        style={btStyle}
        onClick={onClose}
     />
   </div>
-)
-
+);
 
 export default RowButtons
