683cbac094993034ec7bcb8f6bef416ca86a188e
Yevhenii Molodyi <eugmolody@gmail.com>
2022-09-07 12:53:03 +0300

    Edit InputRandom style

--- js/components/panes/InputRandom.js
+++ js/components/panes/InputRandom.js
@@ -7,41 +7,39 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _react = require("react");
-
 var _Atoms = _interopRequireDefault(require("../zhn-atoms/Atoms"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var S = {
-  ROOT: {
-    height: 60,
-    paddingTop: 8
-  },
-  SPAN: {
-    paddingLeft: 16,
-    fontWeight: 'bold'
-  },
-  BT: {
-    marginLeft: 8
-  }
+var S_DIV = {
+  height: 60,
+  paddingTop: 8
+},
+    S_SPAN = {
+  paddingLeft: 16,
+  fontWeight: 'bold'
+},
+    S_BT = {
+  marginLeft: 8
 };
-var InputRandom = /*#__PURE__*/(0, _react.forwardRef)(function (_ref, ref) {
+
+var InputRandom = function InputRandom(_ref) {
   var TS = _ref.TS,
       onEnter = _ref.onEnter;
   return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-    style: S.ROOT,
+    style: S_DIV,
     children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-      style: S.SPAN,
+      style: S_SPAN,
       children: "Random Word"
     }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Atoms["default"].FlatButton, {
-      rootStyle: (0, _extends2["default"])({}, TS.BT.FLAT, S.BT),
+      rootStyle: (0, _extends2["default"])({}, TS.BT.FLAT, S_BT),
       clDiv: TS.BT.CL_FLAT_DIV,
       caption: "Load",
       onClick: onEnter
     })]
   });
-});
+};
+
 var _default = InputRandom;
 exports["default"] = _default;
 //# sourceMappingURL=InputRandom.js.map
--- src/components/panes/InputRandom.jsx
+++ src/components/panes/InputRandom.jsx
@@ -1,33 +1,32 @@
-import { forwardRef } from 'react';
-
 import A from '../zhn-atoms/Atoms';
 
-const S = {
-  ROOT: {
-    height: 60,
-    paddingTop: 8,
-  },
-  SPAN: {
-    paddingLeft: 16,
-    fontWeight: 'bold'
- },
- BT: {
-   marginLeft: 8
- }
+const S_DIV = {
+  height: 60,
+  paddingTop: 8,
+}
+, S_SPAN = {
+  paddingLeft: 16,
+  fontWeight: 'bold'
+}
+, S_BT = {
+  marginLeft: 8
 };
 
-const InputRandom = forwardRef(({ TS, onEnter }, ref) => (
-  <div style={S.ROOT}>
-    <span style={S.SPAN}>
+const InputRandom = ({
+  TS,
+  onEnter
+}) => (
+  <div style={S_DIV}>
+    <span style={S_SPAN}>
       Random Word
     </span>
     <A.FlatButton
-      rootStyle={{...TS.BT.FLAT, ...S.BT}}
+      rootStyle={{...TS.BT.FLAT, ...S_BT}}
       clDiv={TS.BT.CL_FLAT_DIV}
       caption="Load"
       onClick={onEnter}
     />
   </div>
-));
+);
 
 export default InputRandom
