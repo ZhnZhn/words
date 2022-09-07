6730a95182d9ef4d6d6dcf33baeccab88647a888
Yevhenii Molodyi <eugmolody@gmail.com>
2022-09-07 12:37:19 +0300

    Replace PasswordField class component by function component

--- js/components/zhn-m-input/PasswordField.js
+++ js/components/zhn-m-input/PasswordField.js
@@ -7,191 +7,162 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
+var _uiApi = require("../uiApi");
 
-var _react = require("react");
+var _useBool2 = _interopRequireDefault(require("../hooks/useBool"));
+
+var _useRerender = _interopRequireDefault(require("../hooks/useRerender"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var CL = {
-  SELECT: 'm-select',
-  LABEL: 'm-select__label',
-  DIV: 'm-textfield-input__div',
-  INPUT: 'm-textfield-input',
-  INPUT_LINE: 'm-input__line',
-  INPUT_MSG_ERR: 'm-input__msg-err'
-};
-var S = {
-  LABEL_TO_INPUT: {
-    transform: 'scale(1) translate(0px, -6px)'
-  },
-  LABEL_ON_ERROR: {
-    color: '#f44336'
-  },
-  LINE_ERROR: {
-    borderBottom: '2px solid #f44336'
-  },
-  LINE_AFTER_ENTER: {
-    borderBottom: '2px solid greenyellow'
-  }
+var CL_SELECT = 'm-select',
+    CL_LABEL = CL_SELECT + "__label",
+    CL_INPUT = 'm-textfield-input',
+    CL_INPUT_DIV = CL_INPUT + "__div",
+    M_INPUT = 'm-input',
+    CL_INPUT_LINE = M_INPUT + "__line",
+    CL_INPUT_MSG_ERR = M_INPUT + "__msg-err";
+var S_LABEL_TO_INPUT = {
+  transform: 'scale(1) translate(0px, -6px)'
+},
+    S_LABEL_ON_ERROR = {
+  color: '#f44336'
+},
+    S_LINE_ERROR = {
+  borderBottom: '2px solid #f44336'
+},
+    S_LINE_AFTER_ENTER = {
+  borderBottom: '2px solid greenyellow'
 };
 
 var _crId = function _crId(_ref) {
   var name = _ref.name;
-  return name + '_' + Math.random().toString(36).substr(2, 5);
+  return (name || 'pwd') + '_' + Math.random().toString(36).substr(2, 5);
 };
 
-var PasswordField = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(PasswordField, _Component);
+var _isValue = function _isValue(elInput) {
+  return elInput ? !!elInput.value : false;
+};
 
-  function PasswordField(props) {
-    var _this;
+var FN_TRUE = function FN_TRUE() {
+  return true;
+},
+    FN_NOOP = function FN_NOOP() {};
 
-    _this = _Component.call(this, props) || this;
+var PasswordField = (0, _uiApi.forwardRef)(function (props, ref) {
+  var _refInput = (0, _uiApi.useRef)(),
+      _refWasEnter = (0, _uiApi.useRef)(false),
+      _id = (0, _uiApi.useState)(function () {
+    return _crId(props);
+  })[0],
+      rootStyle = props.rootStyle,
+      caption = props.caption,
+      name = props.name,
+      _props$maxLength = props.maxLength,
+      maxLength = _props$maxLength === void 0 ? "32" : _props$maxLength,
+      _props$errorMsg = props.errorMsg,
+      errorMsg = _props$errorMsg === void 0 ? '' : _props$errorMsg,
+      _props$onTest = props.onTest,
+      onTest = _props$onTest === void 0 ? FN_TRUE : _props$onTest,
+      _props$onEnter = props.onEnter,
+      onEnter = _props$onEnter === void 0 ? FN_NOOP : _props$onEnter,
+      _useState = (0, _uiApi.useState)(''),
+      value = _useState[0],
+      setValue = _useState[1],
+      _useBool = (0, _useBool2["default"])(),
+      isFocus = _useBool[0],
+      setFocused = _useBool[1],
+      setNotFocused = _useBool[2],
+      rerender = (0, _useRerender["default"])()[1],
+      _hInputChange = (0, _uiApi.useCallback)(function (evt) {
+    setValue(evt.target.value.trim());
+  }, []),
+      _hKeyDown = (0, _uiApi.useCallback)(function (evt) {
+    var keyCode = evt.keyCode;
 
-    _this._hFocusInput = function () {
-      _this.isFocus = true;
+    if (keyCode === 46) {
+      setValue('');
+    } else if (keyCode === 13) {
+      evt.preventDefault();
+      onEnter(event.target.value);
+      (0, _uiApi.setRefValue)(_refWasEnter, true);
+      rerender();
+    }
+  }, []); // onEnter, rerender
 
-      _this.forceUpdate();
-    };
+  /*eslint-enable react-hooks/exhaustive-deps */
 
-    _this._hBlurInput = function () {
-      _this.isFocus = false;
 
-      _this.forceUpdate();
-    };
+  (0, _uiApi.useImperativeHandle)(ref, function () {
+    return {
+      getValue: function getValue() {
+        var _elInput = (0, _uiApi.getRefValue)(_refInput);
 
-    _this._hInputChange = function (event) {
-      _this.setState({
-        value: event.target.value.trim()
-      });
-    };
-
-    _this._clearAttrValue = function () {
-      _this._clearId = setTimeout(function () {
-        var _input = _this._input;
-
-        if (_input && _input.hasAttribute('value')) {
-          _input.removeAttribute('value');
-        }
-      });
-    };
-
-    _this._clearWasEnter = function () {
-      _this._wasEnter = false;
-    };
-
-    _this._hKeyDown = function (event) {
-      if (event.keyCode === 46) {
-        _this.setState({
-          value: ''
-        });
-      } else if (event.keyCode === 13) {
-        event.preventDefault();
-
-        _this.props.onEnter(event.target.value);
-
-        _this._wasEnter = true;
-
-        _this.forceUpdate(_this._clearWasEnter);
+        return _elInput && _elInput.value;
       }
     };
+  });
+  (0, _uiApi.useEffect)(function () {
+    var _clearId = setTimeout(function () {
+      var _input = (0, _uiApi.getRefValue)(_refInput);
 
-    _this._isValue = function () {
-      return _this._input ? !!_this._input.value : false;
-    };
-
-    _this._refInput = function (node) {
-      return _this._input = node;
-    };
-
-    _this._wasEnter = false;
-    _this.isFocus = false;
-    _this._id = _crId(props);
-    _this.state = {
-      value: ''
-    };
-    return _this;
-  }
-
-  var _proto = PasswordField.prototype;
-
-  _proto.componentWillUnmound = function componentWillUnmound() {
-    clearTimeout(this._clearId);
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        rootStyle = _this$props.rootStyle,
-        caption = _this$props.caption,
-        name = _this$props.name,
-        maxLength = _this$props.maxLength,
-        _this$props$errorMsg = _this$props.errorMsg,
-        errorMsg = _this$props$errorMsg === void 0 ? '' : _this$props$errorMsg,
-        onTest = _this$props.onTest,
-        value = this.state.value,
-        isPassTest = onTest(value),
-        _labelStyle = this._isValue() || this.isFocus ? null : S.LABEL_TO_INPUT,
-        _labelErrStyle = isPassTest ? null : S.LABEL_ON_ERROR,
-        _lineStyle = isPassTest ? this._wasEnter ? S.LINE_AFTER_ENTER : void 0 : S.LINE_ERROR;
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-      className: CL.SELECT,
-      style: rootStyle,
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("label", {
-        className: CL.LABEL,
-        style: (0, _extends2["default"])({}, _labelStyle, _labelErrStyle),
-        htmlFor: this._id,
-        children: caption
-      }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-        className: CL.DIV,
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("input", {
-          hidden: true,
-          autoComplete: "username",
-          value: name,
-          readOnly: true
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)("input", {
-          ref: this._refInput,
-          id: this._id,
-          type: "password",
-          autoComplete: "current-password",
-          className: CL.INPUT,
-          maxLength: maxLength,
-          value: value,
-          onChange: this._hInputChange,
-          onKeyDown: this._hKeyDown,
-          onFocus: this._hFocusInput,
-          onBlur: this._hBlurInput
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-          className: CL.INPUT_LINE,
-          style: _lineStyle
-        }), !isPassTest && /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-          className: CL.INPUT_MSG_ERR,
-          children: errorMsg
-        })]
-      })]
+      if (_input && _input.hasAttribute('value')) {
+        _input.removeAttribute('value');
+      }
     });
-  };
 
-  _proto.componentDidUpdate = function componentDidUpdate() {
-    this._clearAttrValue();
-  };
+    return function () {
+      return clearTimeout(_clearId);
+    };
+  });
+  (0, _uiApi.useEffect)(function () {
+    if ((0, _uiApi.getRefValue)(_refWasEnter)) {
+      (0, _uiApi.setRefValue)(_refWasEnter, false);
+    }
+  });
 
-  _proto.getValue = function getValue() {
-    return this._input && this._input.value;
-  };
+  var _isPassTest = onTest(value),
+      _labelStyle = _isValue((0, _uiApi.getRefValue)(_refInput)) || isFocus ? null : S_LABEL_TO_INPUT,
+      _labelErrStyle = _isPassTest ? null : S_LABEL_ON_ERROR,
+      _lineStyle = _isPassTest ? (0, _uiApi.getRefValue)(_refWasEnter) ? S_LINE_AFTER_ENTER : void 0 : S_LINE_ERROR;
 
-  return PasswordField;
-}(_react.Component);
-
-PasswordField.defaultProps = {
-  name: 'pwd',
-  maxLength: "32",
-  onTest: function onTest() {
-    return true;
-  },
-  onEnter: function onEnter() {}
-};
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    className: CL_SELECT,
+    style: rootStyle,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("label", {
+      className: CL_LABEL,
+      style: (0, _extends2["default"])({}, _labelStyle, _labelErrStyle),
+      htmlFor: _id,
+      children: caption
+    }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      className: CL_INPUT_DIV,
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("input", {
+        hidden: true,
+        autoComplete: "username",
+        value: name,
+        readOnly: true
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("input", {
+        ref: _refInput,
+        id: _id,
+        type: "password",
+        autoComplete: "current-password",
+        className: CL_INPUT,
+        maxLength: maxLength,
+        value: value,
+        onChange: _hInputChange,
+        onKeyDown: _hKeyDown,
+        onFocus: setFocused,
+        onBlur: setNotFocused
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+        className: CL_INPUT_LINE,
+        style: _lineStyle
+      }), !_isPassTest && /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+        className: CL_INPUT_MSG_ERR,
+        children: errorMsg
+      })]
+    })]
+  });
+});
 var _default = PasswordField;
 exports["default"] = _default;
 //# sourceMappingURL=PasswordField.js.map
--- src/components/zhn-m-input/PasswordField.jsx
+++ src/components/zhn-m-input/PasswordField.jsx
@@ -1,171 +1,173 @@
-import { Component } from 'react'
+import {
+  forwardRef,
+  useRef,
+  useState,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  getRefValue,
+  setRefValue
+} from '../uiApi';
 
-const CL = {
-  SELECT: 'm-select',
-  LABEL: 'm-select__label',
-  DIV: 'm-textfield-input__div',
-  INPUT: 'm-textfield-input',
-  INPUT_LINE: 'm-input__line',
-  INPUT_MSG_ERR: 'm-input__msg-err'
+import useBool from '../hooks/useBool';
+import useRerender from '../hooks/useRerender';
+
+const CL_SELECT = 'm-select'
+, CL_LABEL = `${CL_SELECT}__label`
+, CL_INPUT = 'm-textfield-input'
+, CL_INPUT_DIV = `${CL_INPUT}__div`
+, M_INPUT = 'm-input'
+, CL_INPUT_LINE = `${M_INPUT}__line`
+, CL_INPUT_MSG_ERR = `${M_INPUT}__msg-err`
+
+const S_LABEL_TO_INPUT = {
+  transform: 'scale(1) translate(0px, -6px)'
+}
+, S_LABEL_ON_ERROR = {
+  color: '#f44336'
+}
+, S_LINE_ERROR = {
+  borderBottom: '2px solid #f44336'
+}
+, S_LINE_AFTER_ENTER = {
+  borderBottom: '2px solid greenyellow'
 };
 
-const S = {
-  LABEL_TO_INPUT: {
-     transform: 'scale(1) translate(0px, -6px)'
-  },
-  LABEL_ON_ERROR: {
-    color: '#f44336'
-  },
-  LINE_ERROR: {
-    borderBottom: '2px solid #f44336'
-  },
-  LINE_AFTER_ENTER: {
-    borderBottom: '2px solid greenyellow'
-  }
-};
-
-const _crId = ({ name }) => name
+const _crId = ({
+  name
+}) => (name || 'pwd')
   + '_'
   + Math.random().toString(36).substr(2, 5);
 
-class PasswordField extends Component {
-  static defaultProps = {
-    name: 'pwd',
-    maxLength: "32",
-    onTest: () => true,
-    onEnter: () => {}
-  }
+const _isValue = (
+  elInput
+) => elInput
+  ? !!elInput.value
+  : false;
 
-  constructor(props){
-    super(props)
-    this._wasEnter = false
-    this.isFocus = false;
-    this._id = _crId(props)
-    this.state = {
-      value: ''
+const FN_TRUE = () => true
+, FN_NOOP = () => {}
+
+const PasswordField = forwardRef((
+  props,
+  ref
+) => {
+  const _refInput = useRef()
+  , _refWasEnter = useRef(false)
+  , _id = useState(() => _crId(props))[0]
+  , {
+    rootStyle,
+    caption,
+    name,
+    maxLength="32",
+    errorMsg='',
+    onTest=FN_TRUE,
+    onEnter=FN_NOOP
+  } = props
+  , [
+    value,
+    setValue
+  ] = useState('')
+  , [
+    isFocus,
+    setFocused,
+    setNotFocused
+  ] = useBool()
+  , rerender = useRerender()[1]
+  , _hInputChange = useCallback(evt => {
+    setValue(evt.target.value.trim())
+  }, [])
+  /*eslint-disable react-hooks/exhaustive-deps */
+  , _hKeyDown = useCallback(evt => {
+    const { keyCode } = evt;
+    if (keyCode === 46){
+      setValue('')
+    } else if (keyCode === 13) {
+      evt.preventDefault()
+      onEnter(event.target.value)
+      setRefValue(_refWasEnter, true)
+      rerender()
     }
-  }
+  }, []);
+  // onEnter, rerender
+  /*eslint-enable react-hooks/exhaustive-deps */
 
-  componentWillUnmound() {
-    clearTimeout(this._clearId)
-  }
+  useImperativeHandle(ref, ()=>({
+    getValue: () => {
+      const _elInput = getRefValue(_refInput);
+      return _elInput && _elInput.value;
+    },
+  }))
 
-  _hFocusInput = () => {
-    this.isFocus = true
-    this.forceUpdate()
-  }
-  _hBlurInput = () => {
-    this.isFocus = false
-    this.forceUpdate()
-  }
-
-  _hInputChange = (event) => {
-    this.setState({
-      value: event.target.value.trim(),
-    })
-  }
-
-  _clearAttrValue = () => {
-    this._clearId = setTimeout(() => {
-      const _input = this._input;
+  useEffect(() => {
+    const _clearId = setTimeout(()=>{
+      const _input = getRefValue(_refInput);
       if (_input && _input.hasAttribute('value')) {
         _input.removeAttribute('value')
       }
     })
-  }
+    return () => clearTimeout(_clearId);
+  })
+  useEffect(() => {
+    if (getRefValue(_refWasEnter)){
+      setRefValue(_refWasEnter, false)
+    }
+  })
 
-  _clearWasEnter = () => {
-    this._wasEnter = false
-  }
+  const _isPassTest = onTest(value)
+  , _labelStyle = _isValue(getRefValue(_refInput)) || isFocus
+      ? null
+      : S_LABEL_TO_INPUT
+  , _labelErrStyle = _isPassTest
+      ? null
+      : S_LABEL_ON_ERROR
+  , _lineStyle = _isPassTest
+      ? getRefValue(_refWasEnter)
+           ? S_LINE_AFTER_ENTER
+           : void 0
+      : S_LINE_ERROR;
 
- _hKeyDown = (event) => {
-   if (event.keyCode === 46){
-     this.setState({ value: '' })
-   } else if (event.keyCode === 13) {
-     event.preventDefault()
-     this.props.onEnter(event.target.value)
-     this._wasEnter = true
-     this.forceUpdate(this._clearWasEnter)
-   }
- }
- _isValue = () => {
-    return this._input
-       ? !!this._input.value
-       : false;
- }
-
- _refInput = node => this._input = node
-
-  render(){
-    const {
-        rootStyle, caption,
-        name,
-        maxLength, errorMsg='',
-        onTest
-      } = this.props
-    , { value } = this.state
-    , isPassTest = onTest(value)
-    , _labelStyle = (this._isValue() || this.isFocus)
-        ? null
-        : S.LABEL_TO_INPUT
-    , _labelErrStyle = isPassTest
-        ? null
-        : S.LABEL_ON_ERROR
-    , _lineStyle = isPassTest
-        ? this._wasEnter
-             ? S.LINE_AFTER_ENTER
-             : void 0
-        : S.LINE_ERROR;
-    return (
-      <div
-        className={CL.SELECT}
-        style={rootStyle}
-      >
-        <label
-          className={CL.LABEL}
-          style={{..._labelStyle, ..._labelErrStyle}}
-          htmlFor={this._id}
-         >
-          {caption}
-        </label>
-        <div className={CL.DIV}>
-          <input
-            hidden={true}
-            autoComplete="username"
-            value={name}
-            readOnly={true}
-          />
-          <input
-            ref = {this._refInput}
-            id={this._id}
-            type="password"
-            autoComplete="current-password"
-            className={CL.INPUT}
-            maxLength={maxLength}
-            value={value}
-            onChange={this._hInputChange}
-            onKeyDown={this._hKeyDown}
-            onFocus={this._hFocusInput}
-            onBlur={this._hBlurInput}
-          />
-          <div className={CL.INPUT_LINE} style={_lineStyle} />
-          {
-             !isPassTest && <div className={CL.INPUT_MSG_ERR}>
-                 {errorMsg}
-               </div>
-          }
-        </div>
+  return (
+    <div
+      className={CL_SELECT}
+      style={rootStyle}
+    >
+      <label
+        className={CL_LABEL}
+        style={{..._labelStyle, ..._labelErrStyle}}
+        htmlFor={_id}
+       >
+        {caption}
+      </label>
+      <div className={CL_INPUT_DIV}>
+        <input
+          hidden={true}
+          autoComplete="username"
+          value={name}
+          readOnly={true}
+        />
+        <input
+          ref={_refInput}
+          id={_id}
+          type="password"
+          autoComplete="current-password"
+          className={CL_INPUT}
+          maxLength={maxLength}
+          value={value}
+          onChange={_hInputChange}
+          onKeyDown={_hKeyDown}
+          onFocus={setFocused}
+          onBlur={setNotFocused}
+        />
+        <div className={CL_INPUT_LINE} style={_lineStyle} />
+        {
+           !_isPassTest && <div className={CL_INPUT_MSG_ERR}>
+               {errorMsg}
+             </div>
+        }
       </div>
-    );
-  }
-
-  componentDidUpdate() {
-    this._clearAttrValue()
-  }
-
-  getValue(){
-    return this._input && this._input.value;
-  }
-}
+    </div>
+  );
+});
 
 export default PasswordField
