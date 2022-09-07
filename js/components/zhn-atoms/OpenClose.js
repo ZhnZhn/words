f65116848ec2c562df15da43944850b81c355b12
Yevhenii Molodyi <eugmolody@gmail.com>
2022-09-07 12:02:05 +0300

    Replace OpenClose class component by function component

--- js/components/zhn-atoms/OpenClose.js
+++ js/components/zhn-atoms/OpenClose.js
@@ -7,13 +7,13 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
+var _uiApi = require("../uiApi");
 
-var _react = require("react");
+var _useToggle2 = _interopRequireDefault(require("../hooks/useToggle"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-//import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types';
 var CL_CAPTION = 'open-close not-selected',
     S_ROOT = {
   backgroundColor: 'inherit',
@@ -40,82 +40,48 @@ var CL_CAPTION = 'open-close not-selected',
 },
     S_NONE = {
   display: 'none'
-};
-var FILL_OPEN = '#9e9e9e',
+},
+    FILL_OPEN = '#9e9e9e',
     FILL_CLOSE = 'transparent',
     PATH_OPEN = "M 2,14 L 14,14 14,2 2,14",
     PATH_CLOSE = "M 2,2 L 14,8 2,14 2,2";
 
-var OpenClose = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(OpenClose, _Component);
+var OpenClose = function OpenClose(_ref) {
+  var _ref$isClose = _ref.isClose,
+      isClose = _ref$isClose === void 0 ? true : _ref$isClose,
+      _ref$fillOpen = _ref.fillOpen,
+      fillOpen = _ref$fillOpen === void 0 ? FILL_OPEN : _ref$fillOpen,
+      _ref$fillClose = _ref.fillClose,
+      fillClose = _ref$fillClose === void 0 ? FILL_CLOSE : _ref$fillClose,
+      style = _ref.style,
+      itemStyle = _ref.itemStyle,
+      captionStyle = _ref.captionStyle,
+      childrenStyle = _ref.childrenStyle,
+      caption = _ref.caption,
+      afterCaptionComp = _ref.afterCaptionComp,
+      isDraggable = _ref.isDraggable,
+      option = _ref.option,
+      onDragStart = _ref.onDragStart,
+      onDragEnter = _ref.onDragEnter,
+      onDragOver = _ref.onDragOver,
+      onDragLeave = _ref.onDragLeave,
+      onDrop = _ref.onDrop,
+      children = _ref.children;
 
-  /*
-  static propTypes = {
-    isClose: PropTypes.bool,
-      style: PropTypes.object,
-    styleNotSelected: PropTypes.object,
-    captionStyle: PropTypes.object,
-    childrenStyle: PropTypes.object,
-      caption: PropTypes.string,
-    fillOpen: PropTypes.string,
-    fillClose: PropTypes.string,
-      isDraggable: PropTypes.bool,
-    option: PropTypes.object,
-    onDragStart: PropTypes.func,
-    onDragEnter: PropTypes.func,
-    onDragOver: PropTypes.func,
-    onDragLeave: PropTypes.func,
-    onDrop: PropTypes.func,
-      children: PropTypes.oneOfType([
-       PropTypes.arrayOf(PropTypes.node),
-       PropTypes.node
-    ])
-  }
-  */
-  function OpenClose(props) {
-    var _this;
+  var _useToggle = (0, _useToggle2["default"])(!isClose),
+      isOpen = _useToggle[0],
+      toggleIsOpen = _useToggle[1],
+      _hKeyDown = (0, _uiApi.useMemo)(function () {
+    return function (evt) {
+      var keyCode = evt.keyCode;
 
-    _this = _Component.call(this, props) || this;
-
-    _this._handleToggle = function () {
-      _this.setState({
-        isOpen: !_this.state.isOpen
-      });
-    };
-
-    _this._handleKeyDown = function (event) {
-      if (event.keyCode === 13 || event.keyCode === 27) {
-        _this._handleToggle();
+      if (keyCode === 13 || keyCode === 27) {
+        toggleIsOpen();
       }
     };
-
-    _this.state = {
-      isOpen: !props.isClose
-    };
-    return _this;
-  }
-
-  var _proto = OpenClose.prototype;
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        style = _this$props.style,
-        itemStyle = _this$props.itemStyle,
-        captionStyle = _this$props.captionStyle,
-        childrenStyle = _this$props.childrenStyle,
-        caption = _this$props.caption,
-        fillOpen = _this$props.fillOpen,
-        fillClose = _this$props.fillClose,
-        afterCaptionComp = _this$props.afterCaptionComp,
-        isDraggable = _this$props.isDraggable,
-        option = _this$props.option,
-        onDragStart = _this$props.onDragStart,
-        onDragEnter = _this$props.onDragEnter,
-        onDragOver = _this$props.onDragOver,
-        onDragLeave = _this$props.onDragLeave,
-        onDrop = _this$props.onDrop,
-        children = _this$props.children,
-        _dndOption = isDraggable ? {
+  }, []),
+      _dndOption = (0, _uiApi.useMemo)(function () {
+    return isDraggable ? {
       draggable: true,
       onDragStart: onDragStart.bind(null, option),
       onDrop: onDrop.bind(null, option),
@@ -123,69 +89,80 @@ var OpenClose = /*#__PURE__*/function (_Component) {
       onDragOver: onDragOver,
       onDragLeave: onDragLeave
     } : void 0;
+  }, [isDraggable, option]),
+      _ref2 = isOpen ? [PATH_OPEN, fillOpen, S_BLOCK, 'show-popup', null] : [PATH_CLOSE, fillClose, S_NONE, null, itemStyle],
+      _pathV = _ref2[0],
+      _fillV = _ref2[1],
+      _styleCollapse = _ref2[2],
+      _classShow = _ref2[3],
+      _itemStyle = _ref2[4];
 
-    var _pathV, _fillV, _styleCollapse, _classShow, _itemStyle;
-
-    if (this.state.isOpen) {
-      _pathV = PATH_OPEN;
-      _fillV = fillOpen;
-      _styleCollapse = S_BLOCK;
-      _classShow = 'show-popup';
-      _itemStyle = null;
-    } else {
-      _pathV = PATH_CLOSE;
-      _fillV = fillClose;
-      _styleCollapse = S_NONE;
-      _classShow = null;
-      _itemStyle = itemStyle;
-    }
-
-    return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-      style: (0, _extends2["default"])({}, S_ROOT, style),
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsxs)("div", (0, _extends2["default"])({
-        role: "button",
-        className: CL_CAPTION,
-        tabIndex: "0",
-        style: (0, _extends2["default"])({}, S_ROOT_CAPTION, _itemStyle),
-        onClick: this._handleToggle,
-        onKeyDown: this._handleKeyDown
-      }, _dndOption, {
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-          style: S_SVG,
-          children: /*#__PURE__*/(0, _jsxRuntime.jsx)("svg", {
-            viewBox: "0 0 16 16",
-            width: "100%",
-            height: "100%",
-            preserveAspectRatio: "none",
-            xmlns: "http://www.w3.org/2000/svg",
-            style: S_INLINE,
-            children: /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
-              d: _pathV,
-              fill: _fillV,
-              strokeWidth: "1",
-              stroke: fillOpen
-            })
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    style: (0, _extends2["default"])({}, S_ROOT, style),
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsxs)("div", (0, _extends2["default"])({
+      role: "button",
+      className: CL_CAPTION,
+      tabIndex: "0",
+      style: (0, _extends2["default"])({}, S_ROOT_CAPTION, _itemStyle),
+      onClick: toggleIsOpen,
+      onKeyDown: _hKeyDown
+    }, _dndOption, {
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+        style: S_SVG,
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)("svg", {
+          viewBox: "0 0 16 16",
+          width: "100%",
+          height: "100%",
+          preserveAspectRatio: "none",
+          xmlns: "http://www.w3.org/2000/svg",
+          style: S_INLINE,
+          children: /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
+            d: _pathV,
+            fill: _fillV,
+            strokeWidth: "1",
+            stroke: fillOpen
           })
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-          style: (0, _extends2["default"])({}, S_CAPTION, captionStyle),
-          children: caption
-        }), afterCaptionComp]
-      })), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-        className: _classShow,
-        style: (0, _extends2["default"])({}, childrenStyle, _styleCollapse),
-        children: children
-      })]
-    });
-  };
-
-  return OpenClose;
-}(_react.Component);
-
-OpenClose.defaultProps = {
-  isClose: true,
-  fillOpen: FILL_OPEN,
-  fillClose: FILL_CLOSE
+        })
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+        style: (0, _extends2["default"])({}, S_CAPTION, captionStyle),
+        children: caption
+      }), afterCaptionComp]
+    })), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      className: _classShow,
+      style: (0, _extends2["default"])({}, childrenStyle, _styleCollapse),
+      children: children
+    })]
+  });
 };
+/*
+OpenClose.propTypes = {
+  isClose: PropTypes.bool,
+
+  style: PropTypes.object,
+  styleNotSelected: PropTypes.object,
+  captionStyle: PropTypes.object,
+  childrenStyle: PropTypes.object,
+
+  caption: PropTypes.string,
+  fillOpen: PropTypes.string,
+  fillClose: PropTypes.string,
+
+  isDraggable: PropTypes.bool,
+  option: PropTypes.object,
+  onDragStart: PropTypes.func,
+  onDragEnter: PropTypes.func,
+  onDragOver: PropTypes.func,
+  onDragLeave: PropTypes.func,
+  onDrop: PropTypes.func,
+
+  children: PropTypes.oneOfType([
+     PropTypes.arrayOf(PropTypes.node),
+     PropTypes.node
+  ])
+}
+*/
+
+
 var _default = OpenClose;
 exports["default"] = _default;
 //# sourceMappingURL=OpenClose.js.map
--- src/components/zhn-atoms/OpenClose.jsx
+++ src/components/zhn-atoms/OpenClose.jsx
@@ -1,5 +1,9 @@
-import { Component } from 'react'
-//import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types';
+import {
+  useMemo
+} from '../uiApi';
+
+import useToggle from '../hooks/useToggle';
 
 const CL_CAPTION = 'open-close not-selected'
 , S_ROOT = {
@@ -21,148 +25,137 @@ const CL_CAPTION = 'open-close not-selected'
 , S_BLOCK = { display: 'block' }
 , S_NONE = { display: 'none' }
 
-const FILL_OPEN = '#9e9e9e'
-    , FILL_CLOSE = 'transparent'
-    , PATH_OPEN = "M 2,14 L 14,14 14,2 2,14"
-    , PATH_CLOSE = "M 2,2 L 14,8 2,14 2,2";
+, FILL_OPEN = '#9e9e9e'
+, FILL_CLOSE = 'transparent'
+, PATH_OPEN = "M 2,14 L 14,14 14,2 2,14"
+, PATH_CLOSE = "M 2,2 L 14,8 2,14 2,2";
 
-class OpenClose extends Component {
-  /*
-  static propTypes = {
-    isClose: PropTypes.bool,
-
-    style: PropTypes.object,
-    styleNotSelected: PropTypes.object,
-    captionStyle: PropTypes.object,
-    childrenStyle: PropTypes.object,
-
-    caption: PropTypes.string,
-    fillOpen: PropTypes.string,
-    fillClose: PropTypes.string,
-
-    isDraggable: PropTypes.bool,
-    option: PropTypes.object,
-    onDragStart: PropTypes.func,
-    onDragEnter: PropTypes.func,
-    onDragOver: PropTypes.func,
-    onDragLeave: PropTypes.func,
-    onDrop: PropTypes.func,
-
-    children: PropTypes.oneOfType([
-       PropTypes.arrayOf(PropTypes.node),
-       PropTypes.node
-    ])
-  }
-  */
-
-  static defaultProps = {
-    isClose: true,
-    fillOpen: FILL_OPEN,
-    fillClose: FILL_CLOSE
-  }
-
-  constructor(props){
-    super(props)
-    this.state = {
-      isOpen: !props.isClose
+const OpenClose = ({
+  isClose=true,
+  fillOpen=FILL_OPEN,
+  fillClose=FILL_CLOSE,
+  style,
+  itemStyle,
+  captionStyle,
+  childrenStyle,
+  caption,
+  afterCaptionComp,
+  isDraggable,
+  option,
+  onDragStart,
+  onDragEnter,
+  onDragOver,
+  onDragLeave,
+  onDrop,
+  children
+}) => {
+  const [
+    isOpen,
+    toggleIsOpen
+  ] = useToggle(!isClose)
+  /*eslint-disable react-hooks/exhaustive-deps */
+  , _hKeyDown = useMemo(() => (evt) => {
+    const { keyCode } = evt;
+    if (keyCode === 13 || keyCode === 27) {
+      toggleIsOpen()
     }
-  }
+  }, [])
+  // toggleIsOpen
+  /*eslint-enable react-hooks/exhaustive-deps */
 
-  _handleToggle = () => {
-    this.setState({ isOpen : !this.state.isOpen })
-  }
-  _handleKeyDown = (event) => {
-    if (event.keyCode === 13 || event.keyCode === 27 ) {
-      this._handleToggle()
-    }
-  }
+  /*eslint-disable react-hooks/exhaustive-deps */
+  ,  _dndOption = useMemo(() => isDraggable
+       ? {
+           draggable: true,
+           onDragStart: onDragStart.bind(null, option),
+           onDrop: onDrop.bind(null, option),
+           onDragEnter,
+           onDragOver,
+           onDragLeave
+         }
+      : void 0
+    , [isDraggable, option])
+    // onDragStart, onDrop, onDragEnter, onDragOver, onDragLeave
+  /*eslint-enable react-hooks/exhaustive-deps */
+  , [
+    _pathV,
+    _fillV,
+    _styleCollapse,
+    _classShow,
+    _itemStyle
+  ] = isOpen
+    ? [PATH_OPEN, fillOpen, S_BLOCK, 'show-popup', null]
+    : [PATH_CLOSE, fillClose, S_NONE, null, itemStyle];
 
-  render(){
-    const {
-      style,
-      itemStyle,
-      captionStyle,
-      childrenStyle,
-      caption,
-      fillOpen,
-      fillClose,
-      afterCaptionComp,
-      isDraggable,
-      option,
-      onDragStart,
-      onDragEnter,
-      onDragOver,
-      onDragLeave,
-      onDrop,
-      children
-    } = this.props
-    , _dndOption = isDraggable
-         ? {
-             draggable : true,
-             onDragStart : onDragStart.bind(null, option),
-             onDrop : onDrop.bind(null, option),
-             onDragEnter : onDragEnter,
-             onDragOver : onDragOver,
-             onDragLeave : onDragLeave
-           }
-        : void 0;
-
-    let _pathV, _fillV, _styleCollapse, _classShow, _itemStyle;
-    if (this.state.isOpen){
-      _pathV = PATH_OPEN;
-      _fillV = fillOpen;
-      _styleCollapse = S_BLOCK;
-      _classShow = 'show-popup';
-      _itemStyle = null;
-    } else {
-      _pathV = PATH_CLOSE;
-      _fillV = fillClose;
-      _styleCollapse = S_NONE;
-      _classShow = null;
-      _itemStyle = itemStyle;
-    }
-
-    return (
-      <div style={{...S_ROOT, ...style}}>
-        <div
-           role="button"
-           className={CL_CAPTION}
-           tabIndex="0"
-           style={{...S_ROOT_CAPTION, ..._itemStyle }}
-           onClick={this._handleToggle}
-           onKeyDown={this._handleKeyDown}
-           {..._dndOption}
-         >
-          <div style={S_SVG}>
-             <svg
-                viewBox="0 0 16 16"
-                width="100%"
-                height="100%"
-                preserveAspectRatio="none"
-                xmlns="http://www.w3.org/2000/svg"
-                style={S_INLINE}
-              >
-               <path
-                  d={_pathV}
-                  fill={_fillV}
-                  strokeWidth="1" stroke={fillOpen}
-               />
-             </svg>
-         </div>
-         <span style={{...S_CAPTION, ...captionStyle}} >
-            {caption}
-         </span>
-         {afterCaptionComp}
-       </div>
+  return (
+    <div style={{...S_ROOT, ...style}}>
       <div
-         className={_classShow}
-         style={{ ...childrenStyle, ..._styleCollapse }}
-       >
-        {children}
-      </div>
+         role="button"
+         className={CL_CAPTION}
+         tabIndex="0"
+         style={{...S_ROOT_CAPTION, ..._itemStyle }}
+         onClick={toggleIsOpen}
+         onKeyDown={_hKeyDown}
+         {..._dndOption}
+      >
+        <div style={S_SVG}>
+           <svg
+              viewBox="0 0 16 16"
+              width="100%"
+              height="100%"
+              preserveAspectRatio="none"
+              xmlns="http://www.w3.org/2000/svg"
+              style={S_INLINE}
+            >
+             <path
+                d={_pathV}
+                fill={_fillV}
+                strokeWidth="1"
+                stroke={fillOpen}
+             />
+           </svg>
+       </div>
+       <span style={{...S_CAPTION, ...captionStyle}} >
+          {caption}
+       </span>
+       {afterCaptionComp}
      </div>
-    );
-  }
+    <div
+       className={_classShow}
+       style={{ ...childrenStyle, ..._styleCollapse }}
+     >
+      {children}
+    </div>
+   </div>
+  );
+};
+
+/*
+OpenClose.propTypes = {
+  isClose: PropTypes.bool,
+
+  style: PropTypes.object,
+  styleNotSelected: PropTypes.object,
+  captionStyle: PropTypes.object,
+  childrenStyle: PropTypes.object,
+
+  caption: PropTypes.string,
+  fillOpen: PropTypes.string,
+  fillClose: PropTypes.string,
+
+  isDraggable: PropTypes.bool,
+  option: PropTypes.object,
+  onDragStart: PropTypes.func,
+  onDragEnter: PropTypes.func,
+  onDragOver: PropTypes.func,
+  onDragLeave: PropTypes.func,
+  onDrop: PropTypes.func,
+
+  children: PropTypes.oneOfType([
+     PropTypes.arrayOf(PropTypes.node),
+     PropTypes.node
+  ])
 }
+*/
 
 export default OpenClose
