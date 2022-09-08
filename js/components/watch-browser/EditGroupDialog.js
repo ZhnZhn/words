d21bff166f4575c548dddd0b9ad0620d983805d3
Yevhenii Molodyi <eugmolody@gmail.com>
2022-09-08 11:20:02 +0300

    Replace EditGroupDialog class component by function component

--- js/components/watch-browser/EditGroupDialog.js
+++ js/components/watch-browser/EditGroupDialog.js
@@ -7,11 +7,9 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
+var _memoIsShow = _interopRequireDefault(require("../hoc/memoIsShow"));
 
-var _uiApi = require("../uiApi");
-
-var _withTheme = _interopRequireDefault(require("../hoc/withTheme"));
+var _useTheme = _interopRequireDefault(require("../hoc/useTheme"));
 
 var _Dialog = _interopRequireDefault(require("../dialogs/Dialog.Style"));
 
@@ -39,7 +37,6 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-//import PropTypes from "prop-types";
 var addGroup = _WatchActions["default"].addGroup,
     renameGroup = _WatchActions["default"].renameGroup,
     deleteGroup = _WatchActions["default"].deleteGroup;
@@ -48,96 +45,64 @@ var EDIT_WATCH_COMPLETED = _WatchActions.WatchActionTypes.EDIT_WATCH_COMPLETED,
     ADD_GROUP = _WatchActions.WatchActionTypes.ADD_GROUP,
     RENAME_GROUP = _WatchActions.WatchActionTypes.RENAME_GROUP,
     DELETE_GROUP = _WatchActions.WatchActionTypes.DELETE_GROUP;
+var EditGroupDialog = (0, _memoIsShow["default"])(function (_ref) {
+  var _jsx2;
 
-var EditGroupDialog = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(EditGroupDialog, _Component);
-
-  function EditGroupDialog() {
-    return _Component.apply(this, arguments) || this;
-  }
-
-  var _proto = EditGroupDialog.prototype;
-
-  /*
-  static propTypes = {
-    isShow: PropTypes.bool,
-    store: PropTypes.object,
-    onClose: PropTypes.func
-  }
-  */
-  _proto.shouldComponentUpdate = function shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps !== this.props && nextProps.isShow === this.props.isShow) {
-      return false;
-    }
-
-    return true;
-  };
-
-  _proto.render = function render() {
-    var _jsx2;
-
-    var _this$props = this.props,
-        theme = _this$props.theme,
-        isShow = _this$props.isShow,
-        store = _this$props.store,
-        onClose = _this$props.onClose,
-        TS = theme.createStyle(_Dialog["default"]);
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalDialog["default"], {
-      style: (0, _extends2["default"])({}, TS.R_DIALOG, _Dialog2.S_DIALOG),
-      captionStyle: TS.BROWSER_CAPTION,
-      caption: "Watch Groups Edit",
-      isShow: isShow,
-      isWithButton: false,
-      onClose: onClose,
-      children: /*#__PURE__*/(0, _jsxRuntime.jsxs)(_TabPane["default"], {
-        width: _Dialog2.TAB_PANE_WIDTH,
-        tabStyle: _Dialog2.S_TABS,
-        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_Tab["default"], {
-          title: "Create",
-          style: TS.TAB,
-          children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_GroupAddPane["default"], {
-            store: store,
-            inputStyle: TS.INPUT,
-            btStyle: TS.BT.FLAT_ROOT,
-            actionCompleted: EDIT_WATCH_COMPLETED,
-            actionFailed: EDIT_WATCH_FAILED,
-            forActionType: ADD_GROUP,
-            msgOnIsEmptyName: _MsgWatch.emptyName,
-            onCreate: addGroup,
-            onClose: onClose
-          })
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Tab["default"], {
-          title: "Rename",
-          style: TS.TAB,
-          children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_GroupEditPane["default"], {
-            store: store,
-            inputStyle: TS.INPUT,
-            btStyle: TS.BT.FLAT_ROOT,
-            actionCompleted: EDIT_WATCH_COMPLETED,
-            actionFailed: EDIT_WATCH_FAILED,
-            forActionType: RENAME_GROUP,
-            msgOnNotSelect: _MsgWatch.notSelected,
-            msgOnIsEmptyName: _MsgWatch.emptyName,
-            onRename: renameGroup,
-            onClose: onClose
-          })
-        }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Tab["default"], {
-          title: "Delete",
-          style: TS.TAB,
-          children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_GroupDeletePane["default"], (_jsx2 = {
-            store: store,
-            inputStyle: TS.INPUT,
-            btStyle: TS.BT.FLAT_ROOT
-          }, _jsx2["store"] = store, _jsx2.actionCompleted = EDIT_WATCH_COMPLETED, _jsx2.forActionType = DELETE_GROUP, _jsx2.msgOnNotSelect = _MsgWatch.notSelected, _jsx2.onDelete = deleteGroup, _jsx2.onClose = onClose, _jsx2))
-        })]
-      })
-    });
-  };
-
-  return EditGroupDialog;
-}(_uiApi.Component);
-
-var _default = (0, _withTheme["default"])(EditGroupDialog);
-
+  var isShow = _ref.isShow,
+      store = _ref.store,
+      onClose = _ref.onClose;
+  var TS = (0, _useTheme["default"])(_Dialog["default"]);
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalDialog["default"], {
+    style: (0, _extends2["default"])({}, TS.R_DIALOG, _Dialog2.S_DIALOG),
+    captionStyle: TS.BROWSER_CAPTION,
+    caption: "Watch Groups Edit",
+    isShow: isShow,
+    isWithButton: false,
+    onClose: onClose,
+    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)(_TabPane["default"], {
+      width: _Dialog2.TAB_PANE_WIDTH,
+      tabStyle: _Dialog2.S_TABS,
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_Tab["default"], {
+        title: "Create",
+        style: TS.TAB,
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_GroupAddPane["default"], {
+          store: store,
+          inputStyle: TS.INPUT,
+          btStyle: TS.BT.FLAT_ROOT,
+          actionCompleted: EDIT_WATCH_COMPLETED,
+          actionFailed: EDIT_WATCH_FAILED,
+          forActionType: ADD_GROUP,
+          msgOnIsEmptyName: _MsgWatch.emptyName,
+          onCreate: addGroup,
+          onClose: onClose
+        })
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Tab["default"], {
+        title: "Rename",
+        style: TS.TAB,
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_GroupEditPane["default"], {
+          store: store,
+          inputStyle: TS.INPUT,
+          btStyle: TS.BT.FLAT_ROOT,
+          actionCompleted: EDIT_WATCH_COMPLETED,
+          actionFailed: EDIT_WATCH_FAILED,
+          forActionType: RENAME_GROUP,
+          msgOnNotSelect: _MsgWatch.notSelected,
+          msgOnIsEmptyName: _MsgWatch.emptyName,
+          onRename: renameGroup,
+          onClose: onClose
+        })
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_Tab["default"], {
+        title: "Delete",
+        style: TS.TAB,
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_GroupDeletePane["default"], (_jsx2 = {
+          store: store,
+          inputStyle: TS.INPUT,
+          btStyle: TS.BT.FLAT_ROOT
+        }, _jsx2["store"] = store, _jsx2.actionCompleted = EDIT_WATCH_COMPLETED, _jsx2.forActionType = DELETE_GROUP, _jsx2.msgOnNotSelect = _MsgWatch.notSelected, _jsx2.onDelete = deleteGroup, _jsx2.onClose = onClose, _jsx2))
+      })]
+    })
+  });
+});
+var _default = EditGroupDialog;
 exports["default"] = _default;
 //# sourceMappingURL=EditGroupDialog.js.map
--- src/components/watch-browser/EditGroupDialog.jsx
+++ src/components/watch-browser/EditGroupDialog.jsx
@@ -1,8 +1,6 @@
-//import PropTypes from "prop-types";
-import { Component } from '../uiApi';
-
-import withTheme from '../hoc/withTheme'
-import styleConfig from '../dialogs/Dialog.Style'
+import memoIsShow from '../hoc/memoIsShow';
+import useTheme from '../hoc/useTheme';
+import styleConfig from '../dialogs/Dialog.Style';
 
 import Actions, { WatchActionTypes as WAT } from '../../flux/actions/WatchActions';
 
@@ -37,87 +35,65 @@ const {
   DELETE_GROUP
 } = WAT;
 
-class EditGroupDialog extends Component {
-  /*
-  static propTypes = {
-    isShow: PropTypes.bool,
-    store: PropTypes.object,
-    onClose: PropTypes.func
-  }
-  */
+const EditGroupDialog = memoIsShow(({
+  isShow,
+  store,
+  onClose
+}) => {
+  const TS = useTheme(styleConfig);
+  return (
+    <ModalDialog
+       style={{...TS.R_DIALOG, ...S_DIALOG}}
+       captionStyle={TS.BROWSER_CAPTION}
+       caption="Watch Groups Edit"
+       isShow={isShow}
+       isWithButton={false}
+       onClose={onClose}
+    >
+      <TabPane width={TAB_PANE_WIDTH} tabStyle={S_TABS}>
+         <Tab title="Create" style={TS.TAB}>
+           <GroupAddPane
+              store={store}
+              inputStyle={TS.INPUT}
+              btStyle={TS.BT.FLAT_ROOT}
+              actionCompleted={EDIT_WATCH_COMPLETED}
+              actionFailed={EDIT_WATCH_FAILED}
+              forActionType={ADD_GROUP}
+              msgOnIsEmptyName={emptyName}
+              onCreate={addGroup}
+              onClose={onClose}
+            />
+         </Tab>
+         <Tab title="Rename" style={TS.TAB}>
+           <GroupEditPane
+              store={store}
+              inputStyle={TS.INPUT}
+              btStyle={TS.BT.FLAT_ROOT}
+              actionCompleted={EDIT_WATCH_COMPLETED}
+              actionFailed={EDIT_WATCH_FAILED}
+              forActionType={RENAME_GROUP}
+              msgOnNotSelect={notSelected}
+              msgOnIsEmptyName={emptyName}
+              onRename={renameGroup}
+              onClose={onClose}
+           />
+         </Tab>
+         <Tab title="Delete" style={TS.TAB}>
+           <GroupDeletePane
+              store={store}
+              inputStyle={TS.INPUT}
+              btStyle={TS.BT.FLAT_ROOT}
+              store={store}
+              actionCompleted={EDIT_WATCH_COMPLETED}
+              forActionType={DELETE_GROUP}
+              msgOnNotSelect={notSelected}
+              onDelete={deleteGroup}
+              onClose={onClose}
+           />
+         </Tab>
+      </TabPane>
+    </ModalDialog>
+  );
+})
 
-  shouldComponentUpdate(nextProps, nextState){
-    if (nextProps !== this.props
-        && nextProps.isShow === this.props.isShow
-    ) {
-      return false;
-    }
-    return true;
-  }
-
-  render(){
-    const {
-       theme,
-       isShow,
-       store,
-       onClose
-      } = this.props
-    , TS = theme.createStyle(styleConfig);
-
-    return (
-      <ModalDialog
-         style={{...TS.R_DIALOG, ...S_DIALOG}}
-         captionStyle={TS.BROWSER_CAPTION}
-         caption="Watch Groups Edit"
-         isShow={isShow}
-         isWithButton={false}
-         onClose={onClose}
-      >
-        <TabPane width={TAB_PANE_WIDTH} tabStyle={S_TABS}>
-           <Tab title="Create" style={TS.TAB}>
-             <GroupAddPane
-                store={store}
-                inputStyle={TS.INPUT}
-                btStyle={TS.BT.FLAT_ROOT}
-                actionCompleted={EDIT_WATCH_COMPLETED}
-                actionFailed={EDIT_WATCH_FAILED}
-                forActionType={ADD_GROUP}
-                msgOnIsEmptyName={emptyName}
-                onCreate={addGroup}
-                onClose={onClose}
-              />
-           </Tab>
-           <Tab title="Rename" style={TS.TAB}>
-             <GroupEditPane
-                store={store}
-                inputStyle={TS.INPUT}
-                btStyle={TS.BT.FLAT_ROOT}
-                actionCompleted={EDIT_WATCH_COMPLETED}
-                actionFailed={EDIT_WATCH_FAILED}
-                forActionType={RENAME_GROUP}
-                msgOnNotSelect={notSelected}
-                msgOnIsEmptyName={emptyName}
-                onRename={renameGroup}
-                onClose={onClose}
-             />
-           </Tab>
-           <Tab title="Delete" style={TS.TAB}>
-             <GroupDeletePane
-                store={store}
-                inputStyle={TS.INPUT}
-                btStyle={TS.BT.FLAT_ROOT}
-                store={store}
-                actionCompleted={EDIT_WATCH_COMPLETED}
-                forActionType={DELETE_GROUP}
-                msgOnNotSelect={notSelected}
-                onDelete={deleteGroup}
-                onClose={onClose}
-             />
-           </Tab>
-        </TabPane>
-      </ModalDialog>
-    )
-  }
-}
-
-export default withTheme(EditGroupDialog)
+export default EditGroupDialog
