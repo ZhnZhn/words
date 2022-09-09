ee9c55b810b60922fedde322df82ee9e1bb6929b
Yevhenii Molodyi <eugmolody@gmail.com>
2022-09-09 11:16:54 +0300

    Add OptionsStack to OptionsPane

--- js/components/zhn-m-input/OptionsPane.js
+++ js/components/zhn-m-input/OptionsPane.js
@@ -13,62 +13,77 @@ var _ShowHide = _interopRequireDefault(require("../zhn-atoms/ShowHide"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var S = {
-  BT: {
-    display: 'block',
-    width: '100%',
-    textAlign: 'left'
-  },
-  PANE: {
-    position: 'absolute',
-    top: 12,
-    zIndex: '20',
-    width: '100%',
-    paddingTop: 12,
-    paddingBottom: 12,
-    backgroundColor: 'rgb(77, 77, 77)',
-    borderRadius: 2,
-    boxShadow: 'rgba(0, 0, 0, 0.3) 0px 2px 2px 0px, rgba(0, 0, 0, 0.1) 0px 0px 0px 1px'
-  },
-  ITEM: {
-    color: '#80c040'
-  }
+var S_BT = {
+  display: 'block',
+  width: '100%',
+  textAlign: 'left'
+},
+    S_PANE = {
+  position: 'absolute',
+  top: 12,
+  zIndex: '20',
+  width: '100%',
+  paddingTop: 12,
+  paddingBottom: 12,
+  backgroundColor: 'rgb(77, 77, 77)',
+  borderRadius: 2,
+  boxShadow: 'rgba(0, 0, 0, 0.3) 0px 2px 2px 0px, rgba(0, 0, 0, 0.1) 0px 0px 0px 1px'
+},
+    S_ITEM = {
+  color: '#80c040'
 };
 
-var _renderOptions = function _renderOptions(options, currentItem, clItem, itemStyle, onSelect, isShow) {
-  var _ref = currentItem || {},
-      currentCaption = _ref.caption,
-      currentValue = _ref.value;
-
-  return options.map(function (item) {
-    var _style = currentValue && item.value === currentValue || currentCaption && item.caption === currentCaption ? S.ITEM : void 0;
+var OptionsStack = function OptionsStack(_ref) {
+  var options = _ref.options,
+      currentCaption = _ref.currentCaption,
+      currentValue = _ref.currentValue,
+      clItem = _ref.clItem,
+      itemStyle = _ref.itemStyle,
+      onSelect = _ref.onSelect;
+  return (options || []).map(function (item) {
+    var _ref2 = item || {},
+        caption = _ref2.caption,
+        value = _ref2.value,
+        _style = value === currentValue && caption === currentCaption ? S_ITEM : void 0;
 
     return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
       className: clItem,
-      style: (0, _extends2["default"])({}, S.BT, itemStyle, _style),
+      style: (0, _extends2["default"])({}, S_BT, itemStyle, _style),
       onClick: onSelect.bind(null, item),
-      children: item.caption
-    }, item.caption);
+      children: caption
+    }, caption);
   });
 };
 
-var OptionsPane = function OptionsPane(_ref2) {
-  var isShow = _ref2.isShow,
-      rootStyle = _ref2.rootStyle,
-      options = _ref2.options,
-      item = _ref2.item,
-      clItem = _ref2.clItem,
-      itemStyle = _ref2.itemStyle,
-      onSelect = _ref2.onSelect,
-      onClose = _ref2.onClose;
+var OptionsPane = function OptionsPane(_ref3) {
+  var isShow = _ref3.isShow,
+      rootStyle = _ref3.rootStyle,
+      options = _ref3.options,
+      item = _ref3.item,
+      clItem = _ref3.clItem,
+      itemStyle = _ref3.itemStyle,
+      onSelect = _ref3.onSelect,
+      onClose = _ref3.onClose;
+
+  var _ref4 = item || {},
+      caption = _ref4.caption,
+      value = _ref4.value;
+
   return /*#__PURE__*/(0, _jsxRuntime.jsx)(_ModalPane["default"], {
     style: rootStyle,
     isShow: isShow,
     onClose: onClose,
     children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_ShowHide["default"], {
       isShow: isShow,
-      style: (0, _extends2["default"])({}, S.PANE, rootStyle),
-      children: _renderOptions(options, item, clItem, itemStyle, onSelect, isShow)
+      style: (0, _extends2["default"])({}, S_PANE, rootStyle),
+      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(OptionsStack, {
+        options: options,
+        currentCaption: caption,
+        currentValue: value,
+        clItem: clItem,
+        itemStyle: itemStyle,
+        onSelect: onSelect
+      })
     })
   });
 };
--- src/components/zhn-m-input/OptionsPane.jsx
+++ src/components/zhn-m-input/OptionsPane.jsx
@@ -1,60 +1,68 @@
-import ModalPane from '../zhn-moleculs/ModalPane'
-import ShowHide from '../zhn-atoms/ShowHide'
+import ModalPane from '../zhn-moleculs/ModalPane';
+import ShowHide from '../zhn-atoms/ShowHide';
 
-const S = {
-  BT: {
-    display: 'block',
-    width: '100%',
-    textAlign: 'left'
-  },
-  PANE: {
-    position: 'absolute',
-    top: 12,
-    zIndex: '20',
-    width: '100%',
-    paddingTop: 12,
-    paddingBottom: 12,
-    backgroundColor: 'rgb(77, 77, 77)',
-    borderRadius: 2,
-    boxShadow: 'rgba(0, 0, 0, 0.3) 0px 2px 2px 0px, rgba(0, 0, 0, 0.1) 0px 0px 0px 1px'
-  },
-  ITEM: {
-    color: '#80c040'
-  }
+const S_BT = {
+  display: 'block',
+  width: '100%',
+  textAlign: 'left'
+}
+, S_PANE = {
+  position: 'absolute',
+  top: 12,
+  zIndex: '20',
+  width: '100%',
+  paddingTop: 12,
+  paddingBottom: 12,
+  backgroundColor: 'rgb(77, 77, 77)',
+  borderRadius: 2,
+  boxShadow: 'rgba(0, 0, 0, 0.3) 0px 2px 2px 0px, rgba(0, 0, 0, 0.1) 0px 0px 0px 1px'
+}
+, S_ITEM = {
+  color: '#80c040'
 };
 
-const _renderOptions = (
-  options, currentItem,
-  clItem, itemStyle,
-  onSelect, isShow
-) => {
+const OptionsStack = ({
+  options,
+  currentCaption,
+  currentValue,
+  clItem,
+  itemStyle,
+  onSelect
+}) => (options || []).map(item => {
   const {
-    caption:currentCaption,
-    value:currentValue
-  } = currentItem || {};
-  return options.map(item => {
-    const _style = (currentValue && item.value === currentValue)
-      || (currentCaption && item.caption === currentCaption)
-             ? S.ITEM
-             : void 0;
-    return (
-      <button
-        key={item.caption}
-        className={clItem}
-        style={{ ...S.BT, ...itemStyle, ..._style }}
-        onClick={onSelect.bind(null, item)}
-      >
-        {item.caption}
-      </button>
-    );
-  })
-}
+    caption,
+    value
+  } = item || {}
+  , _style = value === currentValue && caption === currentCaption
+      ? S_ITEM
+      : void 0;
+  return (
+    <button
+      key={caption}
+      className={clItem}
+      style={{...S_BT, ...itemStyle, ..._style}}
+      onClick={onSelect.bind(null, item)}
+    >
+      {caption}
+    </button>
+  );
+})
 
 const OptionsPane = ({
-  isShow, rootStyle, options, item,
-  clItem, itemStyle,
-  onSelect, onClose
-}) => (
+  isShow,
+  rootStyle,
+  options,
+  item,
+  clItem,
+  itemStyle,
+  onSelect,
+  onClose
+}) => {
+  const {
+    caption,
+    value
+  } = item || {};
+  return (
   <ModalPane
      style={rootStyle}
      isShow={isShow}
@@ -62,17 +70,19 @@ const OptionsPane = ({
   >
     <ShowHide
        isShow={isShow}
-       style={{ ...S.PANE, ...rootStyle }}
+       style={{...S_PANE, ...rootStyle}}
     >
-      {
-       _renderOptions(
-          options, item,
-          clItem, itemStyle,
-          onSelect, isShow
-        )
-      }
+      <OptionsStack
+        options={options}
+        currentCaption={caption}
+        currentValue={value}
+        clItem={clItem}
+        itemStyle={itemStyle}
+        onSelect={onSelect}
+      />
     </ShowHide>
   </ModalPane>
-);
+  );
+}
 
   export default OptionsPane
