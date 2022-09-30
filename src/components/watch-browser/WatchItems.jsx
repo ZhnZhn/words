927e0475545ef2c4bca500eea8369b9fbd273797
Yevhenii Molodyi <eugmolody@gmail.com>
2022-09-30 13:52:28 +0300

    Add WatchItems function component

---
+++ src/components/watch-browser/WatchItems.jsx
@@ -0,0 +1,49 @@
+import {
+  removeWatchItem
+} from './Handlers';
+
+import {
+  hDragStartItem,
+  hDragOverItem,
+  hDragEnterItem,
+  hDragLeaveItem,
+  hDropItem
+} from './dnd-handlers/DnDItemHandlers';
+
+import WatchItem from './WatchItem';
+
+const _isArr = Array.isArray
+, CL_WATCH_ITEM = 'row__type2-topic not-selected';
+
+const WatchItems = ({
+  isModeEdit,
+  items,
+  groupCaption,
+  listCaption,
+  onClickItem
+}) => _isArr(items) ? items
+  .map(item => {
+    const { caption } = item;
+    return (
+       <WatchItem
+          key={caption}
+          className={CL_WATCH_ITEM}
+          isDraggable={isModeEdit}
+          item={item}
+          option={{
+            groupCaption,
+            listCaption,
+            caption
+          }}
+          onClick={onClickItem}
+          onClose={removeWatchItem}
+          onDragStart={hDragStartItem}
+          onDragOver={hDragOverItem}
+          onDragEnter={hDragEnterItem}
+          onDragLeave={hDragLeaveItem}
+          onDrop={hDropItem}
+       />
+    );
+  }) : null;
+
+export default WatchItems
