a59f53c4ba74172d2a6a1eedd17addd90f3f28b9
Yevhenii Molodyi <eugmolody@gmail.com>
2022-10-24 13:39:18 +0300

    Replace DialogType1 implementation by function component

--- js/components/dialogs/DialogType1.js
+++ js/components/dialogs/DialogType1.js
@@ -5,11 +5,9 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _uiApi = require("../uiApi");
 
-var _withTheme = _interopRequireDefault(require("../hoc/withTheme"));
+var _useTheme = _interopRequireDefault(require("../hoc/useTheme"));
 
 var _Dialog = _interopRequireDefault(require("./Dialog.Style"));
 
@@ -19,90 +17,64 @@ var _TextField = _interopRequireDefault(require("../zhn-m-input/TextField"));
 
 var _jsxRuntime = require("react/jsx-runtime");
 
-var DialogType1 = /*#__PURE__*/function (_Component) {
-  (0, _inheritsLoose2["default"])(DialogType1, _Component);
+var DialogType1 = function DialogType1(_ref) {
+  var isShow = _ref.isShow,
+      caption = _ref.caption,
+      type = _ref.type,
+      source = _ref.source,
+      itemConf = _ref.itemConf,
+      onLoad = _ref.onLoad,
+      onShow = _ref.onShow,
+      onClose = _ref.onClose;
 
-  function DialogType1() {
-    var _this;
+  var _refDialog = (0, _uiApi.useRef)(),
+      _refInputWord = (0, _uiApi.useRef)(),
+      _hLoad = (0, _uiApi.useCallback)(function () {
+    onLoad({
+      type: type,
+      source: source,
+      itemConf: itemConf,
+      symbol: (0, _uiApi.getRefInputValue)(_refInputWord)
+    });
+  }, [type, source, itemConf, onLoad]),
+      _hClose = (0, _uiApi.useCallback)(function () {
+    var _dialogInst = (0, _uiApi.getRefValue)(_refDialog);
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
+    if (_dialogInst) {
+      _dialogInst.focusPrevEl();
     }
 
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
+    onClose();
+  }, [onClose]),
+      _hKeyDown = (0, _uiApi.useCallback)(function (evt) {
+    if (evt.keyCode === 13) {
+      _hLoad();
+    } else if (evt.keyCode === 27) {
+      _hClose();
+    }
+  }, [_hLoad, _hClose]),
+      TS = (0, _useTheme["default"])(_Dialog["default"]);
 
-    _this._handleLoad = function () {
-      var _this$props = _this.props,
-          type = _this$props.type,
-          source = _this$props.source,
-          itemConf = _this$props.itemConf,
-          onLoad = _this$props.onLoad;
-      onLoad({
-        type: type,
-        source: source,
-        itemConf: itemConf,
-        symbol: _this.inputSymbol.getValue()
-      });
-    };
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_DraggableDialog["default"], {
+    ref: _refDialog,
+    style: TS.R_DIALOG,
+    browserCaptionStyle: TS.BROWSER_CAPTION,
+    styleButton: TS.BT,
+    caption: caption,
+    isShow: isShow,
+    onKeyDown: _hKeyDown,
+    onLoad: _hLoad,
+    onShow: onShow,
+    onClose: _hClose,
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_TextField["default"], {
+      ref: _refInputWord,
+      rootStyle: TS.INPUT_ROOT,
+      caption: "Word (Default: Example)",
+      initialValue: "Example"
+    })
+  });
+};
 
-    _this._handleClose = function () {
-      _this.dialogComp.focusPrevEl();
-
-      _this.props.onClose();
-    };
-
-    _this._handleKeyDown = function (event) {
-      if (event.keyCode === 13) {
-        _this._handleLoad();
-      } else if (event.keyCode === 27) {
-        _this._handleClose();
-      }
-    };
-
-    _this._refDialogComp = function (comp) {
-      return _this.dialogComp = comp;
-    };
-
-    _this._refInputWord = function (comp) {
-      return _this.inputSymbol = comp;
-    };
-
-    return _this;
-  }
-
-  var _proto = DialogType1.prototype;
-
-  _proto.render = function render() {
-    var _this$props2 = this.props,
-        theme = _this$props2.theme,
-        isShow = _this$props2.isShow,
-        caption = _this$props2.caption,
-        onShow = _this$props2.onShow,
-        TS = theme.createStyle(_Dialog["default"]);
-    return /*#__PURE__*/(0, _jsxRuntime.jsx)(_DraggableDialog["default"], {
-      ref: this._refDialogComp,
-      rootStyle: TS.R_DIALOG,
-      browserCaptionStyle: TS.BROWSER_CAPTION,
-      styleButton: TS.BT,
-      caption: caption,
-      isShow: isShow,
-      onKeyDown: this._handleKeyDown,
-      onLoad: this._handleLoad,
-      onShow: onShow,
-      onClose: this._handleClose,
-      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_TextField["default"], {
-        ref: this._refInputWord,
-        rootStyle: TS.INPUT_ROOT,
-        caption: "Word (Default: Example)",
-        initialValue: "Example"
-      })
-    });
-  };
-
-  return DialogType1;
-}(_uiApi.Component);
-
-var _default = (0, _withTheme["default"])(DialogType1);
-
+var _default = DialogType1;
 exports["default"] = _default;
 //# sourceMappingURL=DialogType1.js.map
--- src/components/dialogs/DialogType1.jsx
+++ src/components/dialogs/DialogType1.jsx
@@ -1,75 +1,73 @@
-import { Component } from '../uiApi';
+import {
+  useRef,
+  useCallback,
+  getRefValue,
+  getRefInputValue
+} from '../uiApi';
 
-import withTheme from '../hoc/withTheme'
-import styleConfig from './Dialog.Style'
+import useTheme from '../hoc/useTheme';
+import styleConfig from './Dialog.Style';
 
-import DraggableDialog from '../zhn-moleculs/DraggableDialog'
-import TextField from '../zhn-m-input/TextField'
+import DraggableDialog from '../zhn-moleculs/DraggableDialog';
+import TextField from '../zhn-m-input/TextField';
 
-class DialogType1 extends Component {
-  _handleLoad = () => {
-    const {
-      type,
-      source,
-      itemConf,
-      onLoad
-    } = this.props;
-
+const DialogType1 = ({
+  isShow,
+  caption,
+  type,
+  source,
+  itemConf,
+  onLoad,
+  onShow,
+  onClose
+}) => {
+  const _refDialog = useRef()
+  , _refInputWord = useRef()
+  , _hLoad = useCallback(() => {
     onLoad({
       type,
       source,
       itemConf,
-      symbol: this.inputSymbol.getValue()
+      symbol: getRefInputValue(_refInputWord)
     })
-  }
-
-  _handleClose = () => {
-    this.dialogComp.focusPrevEl()
-    this.props.onClose()
-  }
-
-  _handleKeyDown = (event) => {
-    if (event.keyCode === 13){
-      this._handleLoad()
-    } else if (event.keyCode === 27){
-      this._handleClose()
+  }, [type, source, itemConf, onLoad])
+  , _hClose = useCallback(() => {
+    const _dialogInst = getRefValue(_refDialog);
+    if (_dialogInst) {
+      _dialogInst.focusPrevEl()
     }
-  }
+    onClose()
+  }, [onClose])
+  , _hKeyDown = useCallback(evt => {
+    if (evt.keyCode === 13){
+      _hLoad()
+    } else if (evt.keyCode === 27){
+      _hClose()
+    }
+  }, [_hLoad, _hClose])
+  , TS = useTheme(styleConfig);
 
-  _refDialogComp = comp => this.dialogComp = comp
-  _refInputWord = comp => this.inputSymbol = comp
-
-  render(){
-    const {
-      theme,
-      isShow,
-      caption,
-      onShow
-    } = this.props
-    , TS = theme.createStyle(styleConfig);
-
-    return (
-      <DraggableDialog
-           ref={this._refDialogComp}
-           rootStyle={TS.R_DIALOG}
-           browserCaptionStyle={TS.BROWSER_CAPTION}
-           styleButton={TS.BT}
-           caption={caption}
-           isShow={isShow}
-           onKeyDown={this._handleKeyDown}
-           onLoad={this._handleLoad}
-           onShow={onShow}
-           onClose={this._handleClose}
-       >
-         <TextField
-           ref={this._refInputWord}
-           rootStyle={TS.INPUT_ROOT}
-           caption="Word (Default: Example)"
-           initialValue="Example"
-         />
-      </DraggableDialog>
-    );
-  }
+  return (
+    <DraggableDialog
+       ref={_refDialog}
+       style={TS.R_DIALOG}
+       browserCaptionStyle={TS.BROWSER_CAPTION}
+       styleButton={TS.BT}
+       caption={caption}
+       isShow={isShow}
+       onKeyDown={_hKeyDown}
+       onLoad={_hLoad}
+       onShow={onShow}
+       onClose={_hClose}
+     >
+       <TextField
+         ref={_refInputWord}
+         rootStyle={TS.INPUT_ROOT}
+         caption="Word (Default: Example)"
+         initialValue="Example"
+       />
+    </DraggableDialog>
+  );
 }
 
-export default withTheme(DialogType1)
+export default DialogType1
