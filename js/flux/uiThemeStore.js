b1f12ffa89a98b036a56fdf941ba318fef4e37c7
Yevhenii Molodyi <eugmolody@gmail.com>
2023-07-27 17:29:26 +0300

    Add uiThemeStore

---
+++ js/flux/uiThemeStore.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.uiThemeStore = exports.setUiTheme = exports.selectUiTheme = void 0;
+var _zustand = require("zustand");
+var _theme = _interopRequireDefault(require("../components/styles/theme"));
+const selectUiTheme = state => state.uiTheme;
+exports.selectUiTheme = selectUiTheme;
+const _crUiThemeSlice = uiTheme => ({
+  uiTheme
+});
+const uiThemeStore = (0, _zustand.createStore)(set => ({
+  uiTheme: _theme.default,
+  setUiTheme: uiThemeItem => set(state => {
+    const _uiTheme = selectUiTheme(state);
+    if (_uiTheme.getThemeName() === uiThemeItem.value) {
+      return _crUiThemeSlice(_uiTheme);
+    }
+    _uiTheme.setThemeName(uiThemeItem.value);
+    return _crUiThemeSlice({
+      ..._uiTheme
+    });
+  })
+}));
+exports.uiThemeStore = uiThemeStore;
+const setUiTheme = uiThemeStore.getState().setUiTheme;
+exports.setUiTheme = setUiTheme;
+//# sourceMappingURL=uiThemeStore.js.map
---
+++ src/flux/uiThemeStore.js
@@ -0,0 +1,22 @@
+import { createStore } from 'zustand';
+import initialUiTheme  from '../components/styles/theme';
+
+export const selectUiTheme = state => state.uiTheme;
+
+const _crUiThemeSlice = (uiTheme) => ({
+  uiTheme
+});
+
+export const uiThemeStore = createStore(set => ({
+  uiTheme: initialUiTheme,
+  setUiTheme: (uiThemeItem) => set(state => {
+    const _uiTheme = selectUiTheme(state);
+    if (_uiTheme.getThemeName() === uiThemeItem.value) {
+      return _crUiThemeSlice(_uiTheme);
+    }
+    _uiTheme.setThemeName(uiThemeItem.value)
+    return _crUiThemeSlice({..._uiTheme});
+  })
+}))
+
+export const setUiTheme = uiThemeStore.getState().setUiTheme
