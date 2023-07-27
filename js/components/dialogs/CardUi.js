126df83bdd462b506246edf5f06291bc024741f1
Yevhenii Molodyi <eugmolody@gmail.com>
2023-07-27 17:26:31 +0300

    Edit function component CardUi style

--- js/components/dialogs/CardUi.js
+++ js/components/dialogs/CardUi.js
@@ -1,79 +1,74 @@
 "use strict";
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
-exports["default"] = void 0;
-
-var _Comp = _interopRequireDefault(require("../Comp"));
-
-var _jsxRuntime = require("react/jsx-runtime");
-
+exports.default = void 0;
+var _InputSelect = _interopRequireDefault(require("../zhn-m-input/InputSelect"));
+var _SvgCheckBox = _interopRequireDefault(require("../zhn-atoms/SvgCheckBox"));
+var _FlatButton = _interopRequireDefault(require("../zhn-atoms/FlatButton"));
+var _jsxRuntime = require("preact/jsx-runtime");
 //import PropTypes from "prop-types";
-var InputSelect = _Comp["default"].InputSelect,
-    SvgCheckBox = _Comp["default"].SvgCheckBox,
-    FlatButton = _Comp["default"].FlatButton;
-var CL_DIV = 'bt-flat__div';
-var S = {
-  SELECT: {
+
+const CL_DIV = 'bt-flat__div';
+const S_SELECT = {
     ROOT: {
       width: 280
     }
   },
-  CHB_ROW: {
+  S_CHB_ROW = {
     paddingTop: 28,
     paddingLeft: 22
   },
-  TEXT: {
+  S_TEXT = {
     display: 'inline-block',
     paddingLeft: 8,
     fontWeight: 'bold',
     userSelect: 'none'
-  }
-};
-var THEME_OPTIONS = [{
-  caption: 'Grey',
-  value: 'GREY'
-}, {
-  caption: 'Sand',
-  value: 'SAND'
-}, {
-  caption: 'White',
-  value: 'WHITE'
-}],
-    DF_THEME = THEME_OPTIONS[0];
-
-var CardUi = function CardUi(_ref) {
-  var style = _ref.style,
-      buttonsStyle = _ref.buttonsStyle,
-      btStyle = _ref.btStyle,
-      chbStroke = _ref.chbStroke,
-      onSetTheme = _ref.onSetTheme,
-      onCheckAutoSave = _ref.onCheckAutoSave,
-      onUncheckAutoSave = _ref.onUncheckAutoSave,
-      onClose = _ref.onClose;
-  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+  },
+  THEME_OPTIONS = [{
+    caption: 'Grey',
+    value: 'GREY'
+  }, {
+    caption: 'Sand',
+    value: 'SAND'
+  }, {
+    caption: 'White',
+    value: 'WHITE'
+  }],
+  DF_THEME = THEME_OPTIONS[0];
+const CardUi = _ref => {
+  let {
+    style,
+    buttonsStyle,
+    btStyle,
+    chbStroke,
+    onSetTheme,
+    onCheckAutoSave,
+    onUncheckAutoSave,
+    onClose
+  } = _ref;
+  return (0, _jsxRuntime.jsxs)("div", {
     style: style,
-    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(InputSelect, {
-      styleConfig: S.SELECT,
+    children: [(0, _jsxRuntime.jsx)(_InputSelect.default, {
+      styleConfig: S_SELECT,
       caption: "Theme (Default: Grey)",
       initItem: DF_THEME,
       options: THEME_OPTIONS,
       onSelect: onSetTheme
-    }), /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-      style: S.CHB_ROW,
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(SvgCheckBox, {
+    }), (0, _jsxRuntime.jsxs)("div", {
+      style: S_CHB_ROW,
+      children: [(0, _jsxRuntime.jsx)(_SvgCheckBox.default, {
         initialValue: true,
         stroke: chbStroke,
         onCheck: onCheckAutoSave,
         onUnCheck: onUncheckAutoSave
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-        style: S.TEXT,
+      }), (0, _jsxRuntime.jsx)("span", {
+        style: S_TEXT,
         children: "AutoSave on Add to Watch List"
       })]
-    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    }), (0, _jsxRuntime.jsx)("div", {
       style: buttonsStyle,
-      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(FlatButton, {
+      children: (0, _jsxRuntime.jsx)(_FlatButton.default, {
         rootStyle: btStyle,
         clDiv: CL_DIV,
         caption: "Close",
@@ -83,6 +78,7 @@ var CardUi = function CardUi(_ref) {
     })]
   });
 };
+
 /*
 CardUi.propTypes = {
   style: PropTypes.object,
@@ -94,8 +90,6 @@ CardUi.propTypes = {
   onClose: PropTypes.func
 }
 */
-
-
 var _default = CardUi;
-exports["default"] = _default;
+exports.default = _default;
 //# sourceMappingURL=CardUi.js.map
--- src/components/dialogs/CardUi.jsx
+++ src/components/dialogs/CardUi.jsx
@@ -1,64 +1,56 @@
 //import PropTypes from "prop-types";
-
-import Comp from '../Comp'
-
-const {
-  InputSelect,
-  SvgCheckBox,
-  FlatButton
-} = Comp;
+import InputSelect from '../zhn-m-input/InputSelect';
+import SvgCheckBox from '../zhn-atoms/SvgCheckBox';
+import FlatButton from '../zhn-atoms/FlatButton';
 
 const CL_DIV = 'bt-flat__div';
 
-const S = {
-  SELECT: {
-    ROOT: {
-      width: 280
-    }
-  },
-  CHB_ROW: {
-    paddingTop: 28,
-    paddingLeft: 22
-  },
-  TEXT: {
-    display: 'inline-block',
-    paddingLeft: 8,
-    fontWeight: 'bold',
-    userSelect: 'none'
-  }
-};
-
-const THEME_OPTIONS = [
+const S_SELECT = {
+  ROOT: { width: 280 }
+}
+, S_CHB_ROW = {
+  paddingTop: 28,
+  paddingLeft: 22
+}
+, S_TEXT = {
+  display: 'inline-block',
+  paddingLeft: 8,
+  fontWeight: 'bold',
+  userSelect: 'none'
+}
+, THEME_OPTIONS = [
   { caption: 'Grey', value: 'GREY' },
   { caption: 'Sand', value: 'SAND' },
   { caption: 'White', value: 'WHITE' }
-],
-DF_THEME = THEME_OPTIONS[0];
+]
+, DF_THEME = THEME_OPTIONS[0];
 
 const CardUi = ({
   style,
-  buttonsStyle, btStyle,
+  buttonsStyle,
+  btStyle,
   chbStroke,
   onSetTheme,
-  onCheckAutoSave, onUncheckAutoSave,
+  onCheckAutoSave,
+  onUncheckAutoSave,
   onClose
 }) => (
   <div style={style}>
     <InputSelect
-      styleConfig={S.SELECT}
+      styleConfig={S_SELECT}
       caption="Theme (Default: Grey)"
       initItem={DF_THEME}
       options={THEME_OPTIONS}
       onSelect={onSetTheme}
     />
-    <div style={S.CHB_ROW}>
+    <div style={S_CHB_ROW}>
       <SvgCheckBox
         initialValue={true}
         stroke={chbStroke}
         onCheck={onCheckAutoSave}
         onUnCheck={onUncheckAutoSave}
       />
-      <span style={S.TEXT}>AutoSave on Add to Watch List</span>
+      <span style={S_TEXT}>AutoSave on Add to Watch List</span>
     </div>
     <div style={buttonsStyle}>
       <FlatButton
