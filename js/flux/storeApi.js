9dde521f7142508afd275c5789d36ad57c9c6653
Yevhenii Molodyi <eugmolody@gmail.com>
2023-08-28 18:25:43 +0300

    Add function atom named export to flux storeApi

--- js/flux/storeApi.js
+++ js/flux/storeApi.js
@@ -2,17 +2,39 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
-exports.getStoreApi = exports.fCrUse = exports.createStoreWithSelector = void 0;
+exports.getStoreApi = exports.fCrUse = exports.createStoreWithSelector = exports.atom = void 0;
 var _zustand = require("zustand");
 exports.createStore = _zustand.createStore;
 var _bindTo = require("../utils/bindTo");
 exports.bindTo = _bindTo.bindTo;
 var _middleware = require("zustand/middleware");
 var _useSubscribe = _interopRequireDefault(require("../components/hooks/useSubscribe"));
+var _useRerender = _interopRequireDefault(require("../components/hooks/useRerender"));
 const createStoreWithSelector = crStore => (0, _zustand.createStore)((0, _middleware.subscribeWithSelector)(crStore));
 exports.createStoreWithSelector = createStoreWithSelector;
 const getStoreApi = store => [store.setState, store.getState];
 exports.getStoreApi = getStoreApi;
 const fCrUse = (store, select) => (0, _bindTo.bindTo)(_useSubscribe.default, store, select);
 exports.fCrUse = fCrUse;
+const _isFn = v => typeof v === 'function',
+  _reducerUseAtomValue = (value, crOrValue) => _isFn(crOrValue) ? crOrValue(value) : crOrValue;
+const atom = initialValue => {
+  const _atom = Object.create(null);
+  _atom.value = initialValue;
+  return {
+    useAtomValue: () => {
+      _atom.rerender = (0, _useRerender.default)()[1];
+      return _atom.value;
+    },
+    setValue: crOrValue => {
+      const _prev = _atom.value,
+        _rerender = _atom.rerender;
+      _atom.value = _reducerUseAtomValue(_prev, crOrValue);
+      if (_prev !== _atom.value && _isFn(_rerender)) {
+        _rerender();
+      }
+    }
+  };
+};
+exports.atom = atom;
 //# sourceMappingURL=storeApi.js.map
--- src/flux/storeApi.js
+++ src/flux/storeApi.js
@@ -6,6 +6,7 @@ import { subscribeWithSelector } from 'zustand/middleware';
 import { bindTo } from '../utils/bindTo';
 
 import useSubscribe from '../components/hooks/useSubscribe';
+import useRerender from '../components/hooks/useRerender';
 
 export const createStoreWithSelector = (
   crStore
@@ -22,3 +23,29 @@ export const fCrUse = (
   store,
   select
 ) => bindTo(useSubscribe, store, select);
+
+const _isFn = v => typeof v === 'function'
+, _reducerUseAtomValue = (
+  value,
+  crOrValue
+) => _isFn(crOrValue)
+  ? crOrValue(value)
+  : crOrValue;
+export const atom = (initialValue) => {
+   const _atom = Object.create(null);
+   _atom.value = initialValue
+   return {
+     useAtomValue: () => {
+       _atom.rerender = useRerender()[1]
+       return _atom.value;
+     },
+     setValue: (crOrValue) => {
+       const _prev = _atom.value
+       , _rerender = _atom.rerender;
+       _atom.value = _reducerUseAtomValue(_prev, crOrValue)
+       if (_prev !== _atom.value && _isFn(_rerender)) {
+         _rerender()
+       }
+     }
+   };
+}
