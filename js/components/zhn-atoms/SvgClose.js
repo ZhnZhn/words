5feb3821e7b476c6411d30e749859965d38ae4a1
Yevhenii Molodyi <eugmolody@gmail.com>
2024-06-13 11:24:35 +0300

    Remove color property from SvgClose function component

--- js/components/zhn-atoms/SvgClose.js
+++ js/components/zhn-atoms/SvgClose.js
@@ -2,38 +2,37 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
-exports["default"] = void 0;
+exports.default = void 0;
 var _Svg = _interopRequireDefault(require("./svg/Svg100"));
-var _jsxRuntime = require("react/jsx-runtime");
-var CL_SVG_CLOSE = "svg-close",
-  STROKE_COLOR = '#d64336',
+var _jsxRuntime = require("preact/jsx-runtime");
+const CL_SVG_CLOSE = "svg-close",
   S_SVG = {
     padding: 3
   };
-var SvgClose = function SvgClose(_ref) {
-  var style = _ref.style,
-    tabIndex = _ref.tabIndex,
-    onClose = _ref.onClose;
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)("button", {
+const SvgClose = _ref => {
+  let {
+    style,
+    tabIndex,
+    onClose
+  } = _ref;
+  return (0, _jsxRuntime.jsx)("button", {
     type: "button",
     tabIndex: tabIndex,
     className: CL_SVG_CLOSE,
     style: style,
     onClick: onClose,
-    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)(_Svg["default"], {
+    children: (0, _jsxRuntime.jsxs)(_Svg.default, {
       w: "12",
       style: S_SVG,
       strokeWidth: "2",
-      stroke: STROKE_COLOR,
       strokeLinecap: "round",
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
+      children: [(0, _jsxRuntime.jsx)("path", {
         d: "M 0,0 L 12,12"
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("path", {
+      }), (0, _jsxRuntime.jsx)("path", {
         d: "M 12,0 L 0,12"
       })]
     })
   });
 };
-var _default = SvgClose;
-exports["default"] = _default;
+var _default = exports.default = SvgClose;
 //# sourceMappingURL=SvgClose.js.map
--- src/components/zhn-atoms/SvgClose.jsx
+++ src/components/zhn-atoms/SvgClose.jsx
@@ -1,7 +1,6 @@
 import Svg100 from './svg/Svg100';
 
 const CL_SVG_CLOSE = "svg-close"
-, STROKE_COLOR = '#d64336'
 , S_SVG = { padding: 3 };
 
 const SvgClose = ({
@@ -20,7 +19,6 @@ const SvgClose = ({
         w="12"
         style={S_SVG}
         strokeWidth="2"
-        stroke={STROKE_COLOR}
         strokeLinecap="round"
       >
         <path d="M 0,0 L 12,12" />
