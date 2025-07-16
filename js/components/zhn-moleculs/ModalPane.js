d6052fe0a734d2060dd783574e723ad9a557bbbb
Yevhenii Molodyi <eugmolody@gmail.com>
2025-07-16 18:15:39 +0300

    Add function crPresentationRole from a11yFn to function component ModalPane

--- js/components/zhn-moleculs/ModalPane.js
+++ js/components/zhn-moleculs/ModalPane.js
@@ -3,7 +3,9 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
 exports.default = void 0;
+var _a11yFn = require("../a11yFn");
 var _useClickOutside = _interopRequireDefault(require("../hooks/useClickOutside"));
+var _fUseKey = require("../hooks/fUseKey");
 var _jsxRuntime = require("preact/jsx-runtime");
 const ModalPane = _ref => {
   let {
@@ -12,13 +14,17 @@ const ModalPane = _ref => {
     children,
     onClose
   } = _ref;
-  const _refEl = (0, _useClickOutside.default)(isShow, onClose);
+  const _refEl = (0, _useClickOutside.default)(isShow, onClose),
+    _hKeyEscape = (0, _fUseKey.useKeyEscape)(onClose);
+  /*eslint-disable jsx-a11y/no-static-element-interactions*/
   return (0, _jsxRuntime.jsx)("div", {
+    ...(0, _a11yFn.crPresentationRole)(isShow),
     ref: _refEl,
     style: style,
+    onKeyDown: isShow ? _hKeyEscape : void 0,
     children: children
   });
+  /*eslint-enable jsx-a11y/no-static-element-interactions*/
 };
-var _default = ModalPane;
-exports.default = _default;
+var _default = exports.default = ModalPane;
 //# sourceMappingURL=ModalPane.js.map
--- src/components/zhn-moleculs/ModalPane.jsx
+++ src/components/zhn-moleculs/ModalPane.jsx
@@ -1,4 +1,7 @@
+import { crPresentationRole } from '../a11yFn';
+
 import useClickOutside from '../hooks/useClickOutside';
+import { useKeyEscape } from '../hooks/fUseKey';
 
 const ModalPane = ({
   isShow,
@@ -6,15 +9,20 @@ const ModalPane = ({
   children,
   onClose
 }) => {
-  const _refEl = useClickOutside(isShow, onClose);
+  const _refEl = useClickOutside(isShow, onClose)
+  , _hKeyEscape = useKeyEscape(onClose);
+  /*eslint-disable jsx-a11y/no-static-element-interactions*/
   return (
     <div
+      {...crPresentationRole(isShow)}
       ref={_refEl}
       style={style}
+      onKeyDown={isShow ? _hKeyEscape : void 0}
     >
       {children}
     </div>
   );
+  /*eslint-enable jsx-a11y/no-static-element-interactions*/
 };
 
 export default ModalPane
