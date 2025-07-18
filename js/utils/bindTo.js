7e3eb617e19b858e8bbcfd937883696a8e771f85
Yevhenii Molodyi <eugmolody@gmail.com>
2025-07-18 11:20:17 +0300

    Add function safeBindTo named export to utils bindTo module

--- js/utils/bindTo.js
+++ js/utils/bindTo.js
@@ -1,7 +1,8 @@
 "use strict";
 
 exports.__esModule = true;
-exports.bindTo = void 0;
+exports.safeBindTo = exports.bindTo = void 0;
+var _isTypeFn = require("./isTypeFn");
 const bindTo = function (fn) {
   for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
     args[_key - 1] = arguments[_key];
@@ -9,4 +10,11 @@ const bindTo = function (fn) {
   return fn.bind(null, ...args);
 };
 exports.bindTo = bindTo;
+const safeBindTo = function (fn) {
+  for (var _len2 = arguments.length, args = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+    args[_key2 - 1] = arguments[_key2];
+  }
+  return (0, _isTypeFn.isFn)(fn) ? bindTo(fn, ...args) : void 0;
+};
+exports.safeBindTo = safeBindTo;
 //# sourceMappingURL=bindTo.js.map
--- src/utils/bindTo.js
+++ src/utils/bindTo.js
@@ -1,4 +1,10 @@
+import { isFn } from './isTypeFn';
+
 export const bindTo = (
   fn,
   ...args
 ) => fn.bind(null, ...args)
+
+export const safeBindTo = (fn, ...args) => isFn(fn)
+  ? bindTo(fn, ...args)
+  : void 0
